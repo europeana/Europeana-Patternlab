80750e4ac1cca7802adffac58bc1a05a71cdf052
Tim Harbour <tim@uc48.net>
2015-03-04 10:30:04 +0000

    javascript file re-organisation

---
+++ source/js/site_js/search.js
@@ -0,0 +1,26 @@
+//
+// Site specific js file - Search site
+//
+
+
+/* Namespace for the site */
+var Site = window.Site || {};
+
+/* Create a closure to maintain scope of the '$'
+   and remain compatible with other frameworks.  */
+(function($) {
+
+    // Example function
+    Site.newfunc = function(){
+
+    };
+
+
+
+    //same as $(document).ready();
+    $(function() {
+
+
+
+    });
+})(jQuery);
