08fbce33ee7c01f8d98cf1ba4d0c25847b858013
Andy MacLean <andyjmaclean@gmail.com>
2015-03-31 20:29:52 +0200

    Re: #144 Object page tweaks

--- source/js/dist/application.js
+++ source/js/dist/application.js
@@ -944,25 +944,27 @@ function makeArray( obj ) {
 
 });
 ;{
+
   function initFullDoc(){
     var minImgW    = 300;
     var minScreenW = 500;
-    var isb        = $('input[name="edm_is_shown_by"]');
+    var isb        = $('input[name="js_edm_is_shown_by"]');
 
     if(window.location.href.indexOf('js=1') > -1 && isb.length && isb.val().length && $('body').width() > minScreenW){
+    	
       var isbImgTest = $('<img id="isb_img_test" style="visibility:hidden; max-width:none; position:absolute;">');
 
-      isbImgTest.prependTo('.overview');
+      isbImgTest.prependTo('.object-overview');
 
       imagesLoaded(isbImgTest, function(instance){
         if(instance.elements.length && instance.elements[0].width > minImgW){
 
           console.log('img w: ' + isbImgTest.width());
 
-          $('.actions').prepend($('img.main').unwrap().unwrap());
+          $('.object-image-nav').prepend($('.js-preview'));
 
           isbImgTest.removeAttr('style').removeAttr('id').addClass('main');
-	  isbImgTest.wrap( "<div class='img-frame'></div>" );
+	      isbImgTest.wrap( "<div class='js-img-frame'></div>" );
         }
         else{
           isbImgTest.remove();
@@ -971,7 +973,6 @@ function makeArray( obj ) {
       isbImgTest.attr('src', isb.val());
     }
   }
-
 	
   if(typeof initFullDoc != 'undefined'){
     initFullDoc();
--- source/js/modules/object-images.js
+++ source/js/modules/object-images.js
@@ -1,23 +1,25 @@
 {
+
   function initFullDoc(){
     var minImgW    = 300;
     var minScreenW = 500;
-    var isb        = $('input[name="edm_is_shown_by"]');
+    var isb        = $('input[name="js_edm_is_shown_by"]');
 
     if(window.location.href.indexOf('js=1') > -1 && isb.length && isb.val().length && $('body').width() > minScreenW){
+    	
       var isbImgTest = $('<img id="isb_img_test" style="visibility:hidden; max-width:none; position:absolute;">');
 
-      isbImgTest.prependTo('.overview');
+      isbImgTest.prependTo('.object-overview');
 
       imagesLoaded(isbImgTest, function(instance){
         if(instance.elements.length && instance.elements[0].width > minImgW){
 
           console.log('img w: ' + isbImgTest.width());
 
-          $('.actions').prepend($('img.main').unwrap().unwrap());
+          $('.object-image-nav').prepend($('.js-preview'));
 
           isbImgTest.removeAttr('style').removeAttr('id').addClass('main');
-	  isbImgTest.wrap( "<div class='img-frame'></div>" );
+	      isbImgTest.wrap( "<div class='js-img-frame'></div>" );
         }
         else{
           isbImgTest.remove();
@@ -26,7 +28,6 @@
       isbImgTest.attr('src', isb.val());
     }
   }
-
 	
   if(typeof initFullDoc != 'undefined'){
     initFullDoc();
