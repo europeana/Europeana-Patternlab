2f9e999503ad4fc476c33ef5b154b9fa8423873d
Andy MacLean <andyjmaclean@gmail.com>
2015-04-02 11:28:47 +0200

    Re: #144 prep for demo

--- source/js/modules/object-images.js
+++ source/js/modules/object-images.js
@@ -4,21 +4,37 @@
     var minScreenW = 500;
     var isb        = $('input[name="js_edm_is_shown_by"]');
 
-    if(window.location.href.indexOf('js=1') > -1 && isb.length && isb.val().length && $('body').width() > minScreenW){
+    
+    var initChannel1 = function(){
+        console.log('img w: ' + isbImgTest.width());
+        $('.object-actions').prepend($('.is-smallimage'));
+	    isbImgTest.wrap( "<div class='js-img-frame'></div>" );
+    }
+    var initChannel2 = function(){
+		$(".color-data").show();
+		$('.is-smallimage').hide();
+	    isbImgTest.wrap( "<div class='js-img-frame'></div>" );
+    }
+    
+    
+    if(window.location.href.indexOf('js=') > -1 && isb.length && isb.val().length && $('body').width() > minScreenW){
     	
       var isbImgTest = $('<img id="isb_img_test" style="visibility:hidden; max-width:none; position:absolute;">');
-
       isbImgTest.prependTo('.object-overview');
 
       imagesLoaded(isbImgTest, function(instance){
-        if(instance.elements.length && instance.elements[0].width > minImgW){
+       
+    	  if(instance.elements.length && instance.elements[0].width > minImgW){
 
-          console.log('img w: ' + isbImgTest.width());
+    	      isbImgTest.removeAttr('style').removeAttr('id').addClass('main');
 
-          $('.object-actions').prepend($('.is-smallimage'));
-          
-          isbImgTest.removeAttr('style').removeAttr('id').addClass('main');
-	      isbImgTest.wrap( "<div class='js-img-frame'></div>" );
+	       	  if(window.location.href.indexOf('js=1')>-1) {
+	       		initChannel1();
+	       	  }
+	       	  if(window.location.href.indexOf('js=2')>-1) {
+	       		initChannel2();
+	       	  }
+       	  
         }
         else{
           isbImgTest.remove();
@@ -28,6 +44,7 @@
     }
   }
 
+	
   if(typeof initFullDoc != 'undefined'){
     initFullDoc();
   }
