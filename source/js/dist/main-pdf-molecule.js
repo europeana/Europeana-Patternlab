46938b2a84be8f0bf3a2276aa69eed4c9307f5fa
andyjmaclean <andyjmaclean@gmail.com>
2015-07-09 13:51:14 +0200

    Find quick way to test pdf service - undo

--- source/js/dist/main-pdf-molecule.js
+++ source/js/dist/main-pdf-molecule.js
@@ -14,8 +14,7 @@ require(['jquery'], function($){
         require(['pdf_viewer'], function(viewer){
             viewer.init(
                 $('.pdf-viewer'),
-                'http://www.ceti.gr/carare/CETI_OTX_1_EN.pdf'
-                //'/media/compressed.tracemonkey-pldi-09.pdf'
+                '/media/compressed.tracemonkey-pldi-09.pdf'
                 //'/media/m1990_pafilis.pdf'
             );
         });
