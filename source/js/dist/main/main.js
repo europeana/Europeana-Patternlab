8a4b0405e942d45102968752948ac24fbb8fc68a
andyjmaclean <andyjmaclean@gmail.com>
2015-07-15 11:28:08 +0200

    Re: #281 - removed baseUrl from main.js as it makes cross-site content delivery impossible

--- source/js/dist/main/main.js
+++ source/js/dist/main/main.js
@@ -1,42 +1,43 @@
 require.config({
-    baseUrl: '/js/dist',
     paths: {
-        channels:            'eu/channels',
+        channels:            '../eu/channels',
 
-        featureDetect:       'global/feature-detect',
+        featureDetect:       '../global/feature-detect',
 
-        global:              'eu/global',
-        imagesloaded:        'lib/imagesloaded.pkgd',
-        jquery:              'lib/jquery',
+        global:              '../eu/global',
+        imagesloaded:        '../lib/imagesloaded.pkgd',
+        jquery:              '../lib/jquery',
 
-        menus:               'global/menus',
-        placeholder:         'global/placeholder',
-        resize:              'eu/util/resize',
+        menus:               '../global/menus',
+        placeholder:         '../global/placeholder',
+        resize:              '../eu/util/resize',
 
-        blacklight:          'lib/blacklight/blacklight_all',
+        blacklight:          '../lib/blacklight/blacklight_all',
 
-        search_form:         'eu/search-form',
-        search_home:         'eu/search-home',
-        search_object:       'eu/search-object',
-        eu_carousel:         'eu/eu-carousel',
+        search_form:         '../eu/search-form',
+        search_home:         '../eu/search-home',
+        search_object:       '../eu/search-object',
+        eu_carousel:         '../eu/eu-carousel',
 
-        jqDropdown:          'lib/jquery.dropdown',
-        jqScrollto:          'lib/jquery.scrollTo',
+        jqDropdown:          '../lib/jquery.dropdown',
+        jqScrollto:          '../lib/jquery.scrollTo',
 
-        media_controller:    'eu/media/search-media-controller',
-        media_viewer_pdf:    'eu/media/search-pdf-viewer',
-        media_viewer:        'eu/media/search-media-viewer',
-        pdfjs:               'lib/pdfjs/pdf',
+        media_controller:    '../eu/media/search-media-controller',
+        media_viewer_pdf:    '../eu/media/search-pdf-viewer',
+        media_viewer:        '../eu/media/search-media-viewer',
+        pdfjs:               '../lib/pdfjs/pdf',
 
-        aurora:              'lib/audiocogs/aurora',
-        flac:                'lib/audiocogs/flac',
+        aurora:              '../lib/audiocogs/aurora',
+        flac:                '../lib/audiocogs/flac',
+
         videojs:             '//vjs.zencdn.net/4.12/video',
-        videojs_aurora:      'lib/videojs-aurora/videojs-aurora',
-        videojs_silverlight: 'lib/videojs-silverlight/videojs-silverlight',
 
-        photoswipe:         'lib/photoswipe/photoswipe',
-        photoswipe_ui:      'lib/photoswipe/photoswipe-ui-default',
-        media_viewer_image: 'eu/media/search-image-viewer'
+        videojs_aurora:      '../lib/videojs-aurora/videojs-aurora',
+        videojs_silverlight: '../lib/videojs-silverlight/videojs-silverlight',
+
+        photoswipe:         '../lib/photoswipe/photoswipe',
+        photoswipe_ui:      '../lib/photoswipe/photoswipe-ui-default',
+        media_viewer_image: '../eu/media/search-image-viewer'
     },
     shim: {
         blacklight:     ['jquery'],
