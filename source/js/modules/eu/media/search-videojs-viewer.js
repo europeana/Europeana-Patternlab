4259815779f9c4f44aa3daa2cc32b419e6aa0d4b
dan-nl <d_entous@yahoo.com>
2015-07-15 16:38:45 -0700

    audio/video viewer js re-factor
    combined methods into one audio/video js concept

---
+++ source/js/modules/eu/media/search-videojs-viewer.js
@@ -0,0 +1,75 @@
+define([], function() {
+  'use strict';
+
+  function initialiseViewer( viewer ) {
+    videojs(
+      viewer,
+      {}
+    );
+  }
+
+  function setTechOrder() {
+    var tech_order = viewer.getAttribute('data-tech-order');
+
+    if ( !tech_order ) {
+      console.log('no tech order provdied, thus no need to set it');
+      return;
+    }
+
+    videojs.options.techOrder = [tech_order];
+  }
+
+  function determineMediaViewer() {
+    var
+      viewer = $('audio')[0] || $('video')[0],
+      sourceType = viewer.getElementsByTagName('source')[0].getAttribute('type');
+
+      console.log( viewer );
+    if ( !viewer ) {
+      console.log( 'no viewer available' );
+      return;
+    }
+
+    switch ( sourceType ) {
+      case 'audio/flac':
+        require(['aurora'], function() {
+          require(['flac'], function() {
+            require(['videojs'], function() {
+              require(['videojs_aurora'], function() {
+                setTechOrder();
+                initialiseViewer( viewer );
+              });
+            });
+          });
+        });
+        break;
+
+      case 'video/wmv':
+      case 'video/x-msvideo':
+      case 'video/x-ms-wmv':
+        require(['videojs'], function() {
+          require(['videojs_silverlight'], function() {
+            videojs.options.silverlight.xap = "/js/dist/lib/videojs-silverlight/video-js.xap";
+            setTechOrder();
+            initialiseViewer( viewer );
+          });
+        });
+        break;
+
+      default: {
+        require(['videojs'], function() { initialiseViewer(); });
+      }
+    }
+  }
+
+  function init() {
+    determineMediaViewer();
+    console.log( 'init search videojs viewer' );
+  }
+
+  return {
+    init:function() {
+      init();
+    }
+  };
+});
--- source/js/modules/main/main.js
+++ source/js/modules/main/main.js
@@ -1,45 +1,44 @@
 require.config({
     paths: {
-        channels:            '../eu/channels',
+        channels:               '../eu/channels',
 
-        featureDetect:       '../global/feature-detect',
+        featureDetect:          '../global/feature-detect',
 
-        global:              '../eu/global',
-        imagesloaded:        '../lib/imagesloaded.pkgd',
-        jquery:              '../lib/jquery',
+        global:                 '../eu/global',
+        imagesloaded:           '../lib/imagesloaded.pkgd',
+        jquery:                 '../lib/jquery',
 
-        menus:               '../global/menus',
-        placeholder:         '../global/placeholder',
-        resize:              '../eu/util/resize',
+        menus:                  '../global/menus',
+        placeholder:            '../global/placeholder',
+        resize:                 '../eu/util/resize',
 
-        blacklight:          '../lib/blacklight/blacklight_all',
+        blacklight:             '../lib/blacklight/blacklight_all',
 
-        search_form:         '../eu/search-form',
-        search_home:         '../eu/search-home',
-        search_object:       '../eu/search-object',
-        eu_carousel:         '../eu/eu-carousel',
+        search_form:            '../eu/search-form',
+        search_home:            '../eu/search-home',
+        search_object:          '../eu/search-object',
+        eu_carousel:            '../eu/eu-carousel',
 
-        jqDropdown:          '../lib/jquery.dropdown',
-        jqScrollto:          '../lib/jquery.scrollTo',
+        jqDropdown:             '../lib/jquery.dropdown',
+        jqScrollto:             '../lib/jquery.scrollTo',
 
-        media_controller:    '../eu/media/search-media-controller',
-        media_viewer_pdf:    '../eu/media/search-pdf-viewer',
-        media_viewer_video:  '../eu/media/search-video-viewer',
-        media_viewer_audio:  '../eu/media/search-audio-viewer',
-        media_viewer_image: '../eu/media/search-image-viewer',
+        media_controller:       '../eu/media/search-media-controller',
+        media_viewer_pdf:       '../eu/media/search-pdf-viewer',
+        media_viewer_videojs:   '../eu/media/search-videojs-viewer',
+        media_viewer_image:     '../eu/media/search-image-viewer',
 
-        pdfjs:               '../lib/pdfjs/pdf',
+        pdfjs:                  '../lib/pdfjs/pdf',
 
-        aurora:              '../lib/audiocogs/aurora',
-        flac:                '../lib/audiocogs/flac',
+        aurora:                 '../lib/audiocogs/aurora',
+        flac:                   '../lib/audiocogs/flac',
 
-        videojs:             '//vjs.zencdn.net/4.12/video',
+        videojs:                '//vjs.zencdn.net/4.12/video',
 
-        videojs_aurora:      '../lib/videojs-aurora/videojs-aurora',
-        videojs_silverlight: '../lib/videojs-silverlight/videojs-silverlight',
+        videojs_aurora:         '../lib/videojs-aurora/videojs-aurora',
+        videojs_silverlight:    '../lib/videojs-silverlight/videojs-silverlight',
 
-        photoswipe:         '../lib/photoswipe/photoswipe',
-        photoswipe_ui:      '../lib/photoswipe/photoswipe-ui-default'
+        photoswipe:             '../lib/photoswipe/photoswipe',
+        photoswipe_ui:          '../lib/photoswipe/photoswipe-ui-default'
     },
     shim: {
         blacklight:     ['jquery'],
