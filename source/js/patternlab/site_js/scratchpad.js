db8f3b502ee4381096c4cb1290bb499c7dcd5913
Tim Harbour <tim@uc48.net>
2015-09-03 10:52:12 +0100

    Add in smartmenus
    Added smartmenus into bower components
    Added base styles to nav.scss
    Added scratchpad js file into the only output for patternlab area of
    meta/foot to enable experimental javascript in PL without deployment to
    the application.
    Added Pattern specific json file for navigation_top_accessible to
    enable design/development in isolation from existing global navigation

--- source/js/patternlab/site_js/scratchpad.js
+++ source/js/patternlab/site_js/scratchpad.js
@@ -1,16 +1,24 @@
 
 
 
-$(".js-attribution-widget .button-bar").on("click", "button-bar__button", function (e) {
-
-  var attribution = $(this).data("e-licence-content");
-   alert("Hi!");
-  $(".js-attribution-widget input-attr").addClass("is-active").value(attribution).focus();
 
 
-
-  e.preventDefault();
+/* Create a closure to maintain scope of the '$'
+   and remain compatible with other frameworks.  */
+(function($) {
+    //same as $(document).ready();
+    $(function() {
 
 
 
-});
+    	$('#main-menu').smartmenus({
+			mainMenuSubOffsetX: -1,
+			mainMenuSubOffsetY: 4,
+			subMenusSubOffsetX: 6,
+			subMenusSubOffsetY: -6
+		});
+
+
+
+    });
+})(jQuery);
