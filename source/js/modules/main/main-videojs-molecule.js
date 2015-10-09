9be0e206429bd16dde06c7c911920b0eb5b9cebe
andyjmaclean <andyjmaclean@gmail.com>
2015-10-09 14:04:40 +0200

    molecule fix

--- source/js/modules/main/main-videojs-molecule.js
+++ source/js/modules/main/main-videojs-molecule.js
@@ -1,21 +1,21 @@
 require.config({
-  baseUrl: '/js/dist',
   paths: {
-    jquery:                'lib/jquery',
-    aurora:                'lib/audiocogs/aurora',
-    flac:                  'lib/audiocogs/flac',
-    videojs:               '//vjs.zencdn.net/4.12/video',
-    videojs_aurora:        'lib/videojs-aurora/videojs-aurora',
-    videojs_silverlight:   'lib/videojs-silverlight/videojs-silverlight',
-    media_viewer_videojs:  'eu/media/search-videojs-viewer'
+      jquery:                        '../lib/jquery',
+      aurora:                        '../lib/audiocogs/aurora',
+      flac:                          '../lib/audiocogs/flac',
+
+      videojs:                       '//vjs.zencdn.net/4.12/video',
+      videojs_aurora:                '../lib/videojs-aurora/videojs-aurora',
+      videojs_silverlight:           '../lib/videojs-silverlight/videojs-silverlight',
+      media_viewer_video:          '../eu/media/search-videojs-video'
   },
   shim: {
-    media_viewer_videojs: ['jquery']
+    media_viewer_video: ['jquery']
   }
 });
 
 require(['jquery'], function($){
-  require(['media_viewer_videojs'], function(viewer){
+  require(['media_viewer_video'], function(viewer){
       if(media_item){
           viewer.init(media_item);
       }
