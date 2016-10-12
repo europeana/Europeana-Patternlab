3c55b7f6934d56869f53b048129636fca61e9a2d
andyjmaclean <andyjmaclean@gmail.com>
2016-10-12 16:58:02 +0200

    Re: #1572 - tumblr share test (single image)

--- source/js/modules/eu/search-object.js
+++ source/js/modules/eu/search-object.js
@@ -702,9 +702,12 @@ define(['jquery', 'util_scrollEvents', 'ga', 'mustache', 'util_foldable', 'black
         log(params);
 //        window.open('https://www.tumblr.com/widgets/share/tool' + params, '', 'width=540,height=600');
 
-        var url = 'http://www.tumblr.com/share?v=3&u=' + imageUrl + '&t=' + $('h2.object-title').text();
+//        var url = 'http://www.tumblr.com/share?v=3&u=' + imageUrl + '&t=' + $('h2.object-title').text();
+        var url = 'http://www.tumblr.com/share?v=3&u=' + imageUrl + '&t=' + $('h2.object-title').text() + '&canonicalUrl=' + $('[hreflang="x-default"]').attr('href');
+
         window.open(url);
 
+        http://www.tumblr.com/share/link?url=[uri encoded URL]&amp;name=[title]&amp;description=[description]
         return false;
       })
     };
