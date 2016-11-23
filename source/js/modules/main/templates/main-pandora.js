0ff0c769c2b2d7d14f0bfdea2b2f55d085d6fe93
alena-fedasenka <alena.fedasenka@kb.nl>
2016-11-23 16:10:48 +0100

    #2255, #2256, #2253, ...

--- source/js/modules/main/templates/main-pandora.js
+++ source/js/modules/main/templates/main-pandora.js
@@ -1,14 +1,15 @@
 require.config({
   paths: {
-    eu_tooltip:    '../../eu/tooltip/eu-tooltip',
-    jqDropdown:    '../../lib/jquery/jquery.dropdown',
-    jquery:        '../../lib/jquery/jquery',
-    pandoraPage:   '../../eu/pandora/pandora-page',
-    util_ellipsis: '../../eu/util/ellipsis',
-    util_resize:   '../../eu/util/resize',
-    jush:          '../../lib/jush/jush',
-    smartmenus:    '../../lib/smartmenus/jquery.smartmenus',
-    smartmenus_keyboard: '../../lib/smartmenus/keyboard/jquery.smartmenus.keyboard'
+    eu_tooltip:		'../../eu/tooltip/eu-tooltip',
+    jqDropdown:		'../../lib/jquery/jquery.dropdown',
+    jquery:			'../../lib/jquery/jquery',
+    pandoraPage:	'../../eu/pandora/pandora-page',
+    util_ellipsis:	'../../eu/util/ellipsis',
+    util_resize:	'../../eu/util/resize',
+    jush:			'../../lib/jush/jush',
+    smartmenus:		'../../lib/smartmenus/jquery.smartmenus',
+    smartmenus_keyboard:	'../../lib/smartmenus/keyboard/jquery.smartmenus.keyboard',
+    mustache:		'../../lib/mustache/mustache'
   }
 });
 
