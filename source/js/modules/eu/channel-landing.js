79bee5cc2467668393f0c7fb355ce26a50b2256c
andyjmaclean <andyjmaclean@gmail.com>
2017-01-19 16:00:29 +0100

    Re: #1774 sneak peek model change

--- source/js/modules/eu/channel-landing.js
+++ source/js/modules/eu/channel-landing.js
@@ -165,13 +165,23 @@ define(['jquery', 'util_scrollEvents'], function($, scrollEvents) {
     });
   }
 
-  function loadPreview(data){
+  function loadPreview(){
+
+    var data   = window.sneakPeakData;
+    var random = data[Math.floor((Math.random() * data.length) + 1)];
+
+    if(!random){
+      return;
+    }
+
+    $('#js-sneak-peek-title').html(random.title.replace(random.type + ': ', ''));
+    $('#js-sneak-peek-type').html(random.type);
 
     $.ajax({
       beforeSend: function(xhr) {
         xhr.setRequestHeader("X-CSRF-Token", $('meta[name="csrf-token"]').attr('content'));
       },
-      url: data.loadUrl,
+      url: random.url,
       type: 'GET',
       contentType: "application/json; charset=utf-8",
       success: function(data) {
