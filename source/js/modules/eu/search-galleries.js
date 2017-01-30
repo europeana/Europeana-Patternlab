31bc0bbb2ed2091825fd32d7b33e81c4bd0a61a9
andyjmaclean <andyjmaclean@gmail.com>
2017-01-30 22:48:48 +0100

    Re: #1867 Galleries - various inc. photoswipe

--- source/js/modules/eu/search-galleries.js
+++ source/js/modules/eu/search-galleries.js
@@ -1,30 +1,102 @@
-define(['jquery', 'util_scrollEvents'], function($, scrollEvents) {
+define(['jquery', 'purl'], function($, scrollEvents) {
+
+  var gallery = null;
+  var imgData = [];
 
   function log(msg){
     console.log('search-galleries: ' + msg);
-  };
+  }
 
   function initPage(){
     initMasonry();
     initSocialShare();
-  };
+    initPhotoswipe();
+  }
+
+  function openImage(index){
+    require(['photoswipe', 'photoswipe_ui'], function(PhotoSwipe, PhotoSwipeUI_Default){
+      gallery = new PhotoSwipe($('.pswp')[0], PhotoSwipeUI_Default, imgData, {index: index, history: false});
+      gallery.init();
+    });
+  }
+
+  function getImageIndex(url){
+    for(var i=0; i<imgData.length; i++){
+      if(imgData[i].src == url){
+        return i;
+      }
+    }
+  }
+
+  function initPhotoswipe(){
+    if($('.gallery').length == 0){
+      return;
+    }
+
+    require(['jqImagesLoaded'], function(){
+      require(['photoswipe', 'photoswipe_ui'], function(PhotoSwipe, PhotoSwipeUI_Default){
+
+        var css_path_1      = require.toUrl('../../lib/photoswipe/photoswipe.css'),
+            css_path_2      = require.toUrl('../../lib/photoswipe/default-skin/default-skin.css'),
+            imgUrls         = $.map($('img[data-full-image]'), function(x){return $(x).data('full-image')});
+
+        $('body').append('<div id="img-measure" style="position:absolute; visibility:hidden;">');
+        $('head').append('<link rel="stylesheet" href="' + css_path_1 + '" type="text/css"/>');
+        $('head').append('<link rel="stylesheet" href="' + css_path_2 + '" type="text/css"/>');
+        $('head').append('<style>.pswp__button--share{ display: none; }</style>');
+
+        for(var i=0; i < imgUrls.length; i++){
+          $('#img-measure').append('<img src="' + imgUrls[i] + '">');
+        }
+
+        $('#img-measure').imagesLoaded( function($images, $proper, $broken) {
+          for(var i=0; i< $images.length; i++){
+            var img = $( $images[i] );
+            imgData.push({
+              src: img.attr('src'),
+              h:   img.height(),
+              w:   img.width()
+            });
+
+            log('img.height() = ' + img.height() + ', img.width() = ' + img.width() + ', img.attr(src) = ' + img.attr('src'));
+          }
+
+          require(['purl'], function(){
+            var purl     = $.url(window.location.href);
+            var imgIndex = purl.param('imgIndex');
+            if(imgIndex){
+              openImage(imgIndex);
+            }
+          });
+
+        });
+      });
+    });
+
+    $('.btn-zoom').on('click', function(e){
+      var tgt   = $(e.target);
+      var img   = tgt.closest('.masonry-item').find('img[data-full-image]').data('full-image');
+      var index = getImageIndex(img);
+      openImage(index);
+    });
+  }
 
   function initMasonry(){
 
-    if($('.galleries').length == 0){
+    if($('.masonry-items').length == 0){
       return;
     }
 
     require(['masonry', 'jqImagesLoaded'], function(Masonry){
 
-      masonry = new Masonry( '.galleries', {
-        itemSelector: '.image-set',
+      masonry = new Masonry('.masonry-items', {
+        itemSelector: '.masonry-item',
         columnWidth: '.grid-sizer',
         gutter: '.gutter-sizer',
         percentPosition: true
       });
 
-      $('.galleries').imagesLoaded().progress( function(instance, image){
+      $('.masonry-items').imagesLoaded().progress( function(instance, image){
         masonry.layout();
       }).done( function(){
         masonry.layout();
