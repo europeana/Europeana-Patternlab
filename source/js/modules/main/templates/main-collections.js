4193f13c0748eaf0a55b0392c812d8880992e892
andyjmaclean <andyjmaclean@gmail.com>
2017-03-21 12:59:13 +0100

    Re: #2009 GA configurability

--- source/js/modules/main/templates/main-collections.js
+++ source/js/modules/main/templates/main-collections.js
@@ -1,20 +1,9 @@
 window.GoogleAnalyticsObject = '__ga__';
+
 window.__ga__ = {
-    q: [['create', 'UA-12776629-1', 'auto']],
+    q: [['create', googleAnalyticsKey, 'auto']],
     l: Date.now()
 };
-
-/*
-var release   = null;
-
-var scripts = document.getElementsByTagName('script');
-for (var i=0; i<scripts.length; i++){
-    var v = scripts[i].getAttribute('js-version');
-    if(v){
-        release = v;
-    }
-};
-*/
 
 require.config({
   //urlArgs: "cache=" + (release || Math.random()),
