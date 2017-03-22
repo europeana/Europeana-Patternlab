1401648f7bf8e5c2da20ea370f96fed25158bad9
andyjmaclean <andyjmaclean@gmail.com>
2017-03-22 15:54:06 +0100

    Re: #1935 blogs styling

--- source/js/modules/eu/search-blog.js
+++ source/js/modules/eu/search-blog.js
@@ -102,7 +102,7 @@ define(['jquery'], function($) {
       p = $(p);
 
       if(p.find('img').length == 1){
-          p.find('img').removeAttr('style');
+        p.find('img').removeAttr('style').removeAttr('width').removeAttr('height');
       }
       else if(p.find('img').length == 2){
         var img1 = p.find('img:first');
@@ -116,29 +116,15 @@ define(['jquery'], function($) {
             p.find('img:last').css('float', 'right');
             p.addClass('cf');
           }
+          else{
+            img1.add(img2).removeAttr('style').removeAttr('width').removeAttr('height');
+          }
+        }
+        else{
+          img1.add(img2).removeAttr('style').removeAttr('width').removeAttr('height');
         }
       }
-      /*
-      else if(p.find('img').length == 2 && p.children().length == 2){
-          var x = p.clone();
-          x.find('img').remove();
-          x = x[0].outerHTML.replace('&nbsp;', '').replace(' ', '');
-
-          if($(x).is(':empty')){
-              var w = p.find('img:first').width() + p.find('img:last').width();
-
-              console.log(w  + '  <  ' +  p.width());
-
-              if(w < p.width()){
-                  p.find('img:first').css('float', 'left');
-                  p.find('img:last').css('float', 'right');
-                  p.addClass('cf');
-              }
-          }
-      }
-      */
-  });
-
+    });
   }
 
   return {
