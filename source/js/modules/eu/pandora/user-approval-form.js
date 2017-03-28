815d0f8fccaec6470de2b91771b50c98bc320140
E.Y. Silva Navarrete <e.y.silvanavarrete@europeana.eu>
2017-03-28 17:52:25 +0200

    updating user approval form for organizations autocompleter

--- source/js/modules/eu/pandora/user-approval-form.js
+++ source/js/modules/eu/pandora/user-approval-form.js
@@ -1,49 +1,7 @@
 define(['jquery', 'autocomplete'], function ($) {
 
-    var currentRoles = [],
-        currentKeys = [],
+    var currentKeys = [],
         selectedRoles = [];
-
-    function switchPopUp(term, update, roles, rolesLi) {
-        $('.modal-wrapper').toggleClass('open');
-        $('.page-wrapper').toggleClass('blur');
-
-        // if exist add tag class to avoid duplication.
-        if (update) {
-            $('.modal-wrapper').addClass('updatingRole');
-        }
-
-        if (roles) {
-            var optionsList = '';
-
-            roles[0].roles.map(function (role) {
-                optionsList += '<option value="' + role.name + '">' + role.name + '</option>';
-            });
-
-            if (!arraySearch(currentRoles, term)) {
-                currentRoles[term] = optionsList;
-                selectedRoles.push(term);
-                currentKeys.push(term);
-            }
-
-            $('#availableRoles').html(optionsList);
-        } else {
-            $('#availableRoles').html(rolesLi);
-        }
-
-        $('.currentSelectedOrganization').html(term);
-
-        $('#searchOrganization').val('');
-        $('#searchOrganization').attr('placeholder', 'Search Organizations ...');
-    };
-
-
-    function arraySearch(arr, val) {
-        for (var i = 0; i < arr.length; i++)
-            if (arr[i] === val)
-                return i;
-        return false;
-    };
 
     Array.prototype.exclude = function (list) {
         return this.filter(function (el) {
@@ -56,36 +14,37 @@ define(['jquery', 'autocomplete'], function ($) {
         minChars: 2,
         onSelect: function (e, term, item) {
             var result = $.grep(data, function (e) {
-                return (e.name).toUpperCase() === term;
+                return (e.organizationName).toUpperCase() === term;
             });
-
             switchPopUp(term, false, result);
         },
         renderItem: function (item, search) {
-            // escape special characters
             search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
             var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
-            return '<div class="autocomplete-suggestion" data-val="' + item + '">' + item.replace(re, "<b>$1</b>") + '</div>';
+            return '<div class="autocomplete-suggestion" id="' + item.organizationId
+                +'" data-id="' + item.organizationId + '" data-val="'
+                + item.name + '">' + item.name.replace(re, "<b>$1</b>") + '</div>';
         },
-
         source: function (term, response) {
             var typedSearch = $('#searchOrganization').val().toUpperCase();
-
             var jsonPromise = $.getJSON(
                 // TODO: test URL. Get the right URL for production.
                 // 'https://www.europeana.eu/api/v2/search.json?wskey=api2demo&rows=9&query="' + typedSearch + '"',
                 'http://metis.webdeveloper-tips.com/organizations.json',
+
                 function (data) {
                     var final = [];
                     var items = data.orgs;
 
                     items.map(function (x) {
-                        var stringVal = x.name.toUpperCase();
+                        var stringVal = x.organizationName.toUpperCase();
+                        var obj = {"name": stringVal, "organizationId":x.organizationId };
 
                         if (stringVal.indexOf(typedSearch) !== -1) {
-                            final.push(stringVal);
+                            // console.log(obj);
+                            final.push(obj);
                         } else {
-                            return stringVal;
+                            return obj;
                         }
                     });
 
@@ -98,33 +57,67 @@ define(['jquery', 'autocomplete'], function ($) {
             });
         }
     });
+    function switchPopUp(org, update, roles) {
+        $('.modal-wrapper').toggleClass('open');
+        $('.page-wrapper').toggleClass('blur');
+
+        if (org.orgName) {
+            $('.currentSelectedOrganization').html(org.orgName);
+            $('.currentSelectedOrganization').attr('data-organizationId', org.organizationId);
+        }
+        // if exist add tag class to avoid duplication.
+        if (update) {
+            $('.modal-wrapper').addClass('updatingRole');
+        }
+        if (roles) {
+            var optionsList = '',
+            rolesSel    = "select[data-rolestypeid='" + roles[0].rolesTypeId +"']";
+
+            $('.currentSelectedOrganization').html(roles[0].organizationName);
+            $('.currentSelectedOrganization').attr('data-organizationid', roles[0].organizationId);
+
+            $('#searchOrganization').val('');
+            $('#searchOrganization').attr('placeholder', 'Search Organizations ...');
+            $(rolesSel).removeClass('hidden');
+        } else {
+        }
+    };
 
     function saveData(dat) {
         data = dat;
     };
 
     $('.btn-close').click(function () {
-        switchPopUp();
+        $('.roles').addClass('hidden');
+        switchPopUp(false);
     });
 
     //clicking an already selected organization from the selected box.
     $('.selectedOrganizations').on('click', '.selectedOrganization', function () {
-        var availableRoles = currentRoles[$(this).data('name')],
-            currentRole = $(this).data('role'),
-            indexRole = availableRoles.indexOf(currentRole);
 
-        availableRoles = insert(availableRoles, indexRole - 7, 'selected="selected" ');
+        var orgName     = $(this).attr('data-name'),
+            orgRole     = $(this).attr('data-role'),
+            orgID       = $(this).attr('data-organizationid'),
+            orgRolesId  = $(this).attr('data-rolestypeid'),
+            rolesSel    = "select[data-rolestypeid='" + orgRolesId +"']",
+            currentOrg = {
+                "organizationId": orgID,
+                "orgName": orgName,
+                "orgRole": orgRole
+            };
 
+        $(rolesSel).removeClass('hidden');
 
-        switchPopUp($(this).text(), true, false, availableRoles);
+        $(rolesSel + " option").filter(function() {
+            //may want to use $.trim in here
+            return $(this).text() == orgRole;
+        }).prop('selected', true);
+
+         $('.modal-wrapper').addClass('alreadySelected');
+         switchPopUp(currentOrg);
     });
 
-    function insert(str, index, value) {
-        return str.substr(0, index) + value + str.substr(index);
-    };
-
     $('.selectedOrganizations').on('click', '.removeOrganization', function () {
-
         $(this).parent().parent().remove();
         delete currentRoles[$(this).data('name')];
 
@@ -142,32 +135,26 @@ define(['jquery', 'autocomplete'], function ($) {
 
     // after clicking button the role is assigned to the current organization and the pop-up it's closed
     $('#roleSelection').on('click', function () {
+        var selectedOrg         = $('.currentSelectedOrganization').text(),
+            selectedRole        = $('select.roles').not(".hidden").find(":selected").text(),
+            rolesTypeId         = $('select.roles').not(".hidden").data('rolestypeid'),
+            orgId               = $('.currentSelectedOrganization').attr('data-organizationid'),
+            orgContainer        ='.selectedOrg[id="' + orgId + '"]';
 
-            var selectedOrg = $('.currentSelectedOrganization').text();
-            var selectedRole = $('#availableRoles').find(":selected").text();
+        var updatedOrg = '<a class="selectedOrganization alreadySelected" name="organization" data-role="'
+            + selectedRole + '" data-rolestypeid="' + rolesTypeId + '"  data-name="'
+            + selectedOrg + '" data-organizationid="' + orgId + '">' + selectedOrg
+            + ': ' + selectedRole + '</a> <b><a class="removeOrganization" data-name="'
+            + selectedOrg + '"> X</a></b>';
 
-
-            if (!$('.modal-wrapper').hasClass('updatingRole')) {
-
-                $('.selectedOrganizations')
-                    .append(
-                        '<div class="selectedOrg" id="' + selectedOrg + '"><a class="selectedOrganization alreadySelected" data-role="'
-                        + selectedRole + '" data-name="' + selectedOrg + '">' + selectedOrg
-                        + ': ' + selectedRole + '</a> <b><a class="removeOrganization" data-name="'
-                        + selectedOrg + '"> X</a></b></div>'
-                    );
-            } else {
-                var updatedRolePerOrg = selectedOrg.substring(0, selectedOrg.indexOf(':'));
-                var updatedOrg = '<div class="selectedOrg" id="' + updatedRolePerOrg + '"><a class="selectedOrganization alreadySelected" data-role="'
-                    + selectedRole + '" data-name="' + updatedRolePerOrg + '">' + updatedRolePerOrg
-                    + ': ' + selectedRole + '</a> <b><a class="removeOrganization" data-name="'
-                    + updatedRolePerOrg + '"> X</a></b></div>';
-
-                $("[id='" + updatedRolePerOrg + "']").html(updatedOrg);
-                $('.modal-wrapper').removeClass('updatingRole');
-            }
-
-            switchPopUp();
+        if($('.selectedOrganizations .selectedOrg[id="' + orgId + '"]').length){
+                $(orgContainer).html(updatedOrg);
+                $('.selectOrg[id= ' + orgId + ']').html(updatedOrg);
+        } else {
+                $('.selectedOrganizations').append('<div class="selectedOrg" id="' + orgId + '">' + updatedOrg + '</div>');
         }
-    );
+
+        $('.roles').addClass('hidden');
+        switchPopUp(false);
+    });
 });
