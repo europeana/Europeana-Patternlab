834be728e313e39e5d6e440a9bba700f319538b2
andyjmaclean <andyjmaclean@gmail.com>
2017-05-09 16:37:39 +0200

    Re: #2124 new iframe template / basic breadcrumbs

--- source/js/modules/eu/channels.js
+++ source/js/modules/eu/channels.js
@@ -1,298 +1,298 @@
-define(['jquery', 'search_form', 'smartmenus'], function ($, euSearchForm) {
+define(['jquery', 'smartmenus'], function ($) {
 
-    var promisedPageJS = jQuery.Deferred();
-    var log = function(msg){
-      console.log('Channels: ' + msg);
-    }
+  var promisedPageJS = jQuery.Deferred();
 
-    require(['cookie_disclaimer'], function(cd){
-      cd.init();
+  var log = function(msg){
+    console.log('Channels: ' + msg);
+  };
+
+  require(['cookie_disclaimer'], function(cd){
+    cd.init();
+  });
+
+  require(['smartmenus_keyboard'], function(){
+
+    $('.nav_primary>ul').smartmenus({
+      mainMenuSubOffsetX: -1,
+      mainMenuSubOffsetY: 4,
+      subMenusSubOffsetX: 6,
+      subMenusSubOffsetY: -6,
+      subMenusMaxWidth: null,
+      subMenusMinWidth: null
     });
 
-    require(['smartmenus_keyboard'], function(){
-
-        $('.nav_primary>ul').smartmenus({
-            mainMenuSubOffsetX: -1,
-            mainMenuSubOffsetY: 4,
-            subMenusSubOffsetX: 6,
-            subMenusSubOffsetY: -6,
-            subMenusMaxWidth: null,
-            subMenusMinWidth: null
-        });
-
-        $('#settings-menu').smartmenus({
-            mainMenuSubOffsetX: -62,
-            mainMenuSubOffsetY: 4,
-            subMenusSubOffsetX: 0,
-            subMenusSubOffsetY: -6,
-            subMenusMaxWidth: null,
-            subMenusMinWidth: null
-        });
-
-        $('#browse-menu').smartmenus({
-            mainMenuSubOffsetX: -25,
-            mainMenuSubOffsetY: 4,
-            subMenusSubOffsetX: 0,
-            subMenusSubOffsetY: -6,
-            subMenusMaxWidth: null,
-            subMenusMinWidth: null
-        });
-
-        $('.js-hack-smartmenu a').click(function(){
-            var href = $(this).attr('href');
-            if(href != '#'){
-                window.location = $(this).attr('href');
-            }
-        });
-
-        $('.nav_primary>ul').smartmenus('keyboardSetHotkey', '123', 'shiftKey');
-        $('#settings-menu').smartmenus('keyboardSetHotkey', '123', 'shiftKey');
-
+    $('#settings-menu').smartmenus({
+      mainMenuSubOffsetX: -62,
+      mainMenuSubOffsetY: 4,
+      subMenusSubOffsetX: 0,
+      subMenusSubOffsetY: -6,
+      subMenusMaxWidth: null,
+      subMenusMinWidth: null
     });
 
-    var initCollectionsFilter = function(){
-      require(['purl'], function(){
-        $('#list_sortby').add('#list_filterby').on('change', function(){
-          var val        = $(this).val();
-          var filterName = $(this).data('filter-name');
-          if(!filterName){
-            filterName = $(this).attr('id') == 'list_sortby' ? 'order' : 'theme';
-          }
-          var $url   = $.url();
-          var params = $url.param();
-          
-          params[filterName] = (['*', 'all'].indexOf(val) > -1 ? '' : val);
-          
-          if(!params[filterName]){
-            delete params[filterName];
-          }
-          if(params['page']){
-            delete params['page'];
-          }
-          
-          var newParams = $.param(params);
-          var newUrl = window.location.href.split('?')[0] + (newParams.length == 0 ? '' : '?' + $.param(params));
-          window.location.href = newUrl;
-        });
+    $('#browse-menu').smartmenus({
+      mainMenuSubOffsetX: -25,
+      mainMenuSubOffsetY: 4,
+      subMenusSubOffsetX: 0,
+      subMenusSubOffsetY: -6,
+      subMenusMaxWidth: null,
+      subMenusMinWidth: null
+    });
+
+    $('.js-hack-smartmenu a').click(function(){
+      var href = $(this).attr('href');
+      if(href != '#'){
+        window.location = $(this).attr('href');
+      }
+    });
+
+    $('.nav_primary>ul').smartmenus('keyboardSetHotkey', '123', 'shiftKey');
+    $('#settings-menu').smartmenus('keyboardSetHotkey', '123', 'shiftKey');
+
+  });
+
+  var initCollectionsFilter = function(){
+    require(['purl'], function(){
+      $('#list_sortby').add('#list_filterby').on('change', function(){
+        var val    = $(this).val();
+        var filterName = $(this).data('filter-name');
+        if(!filterName){
+          filterName = $(this).attr('id') == 'list_sortby' ? 'order' : 'theme';
+        }
+        var $url   = $.url();
+        var params = $url.param();
+
+        params[filterName] = (['*', 'all'].indexOf(val) > -1 ? '' : val);
+
+        if(!params[filterName]){
+          delete params[filterName];
+        }
+        if(params['page']){
+          delete params['page'];
+        }
+
+        var newParams = $.param(params);
+        var newUrl = window.location.href.split('?')[0] + (newParams.length == 0 ? '' : '?' + $.param(params));
+        window.location.href = newUrl;
+      });
+    });
+  };
+
+  var initFeedback = function(){
+    if($('.feedback').size()>0){
+      require(['feedback'], function(fb){
+        fb.init($('.feedback'));
       });
     }
+  };
 
-    var initFeedback = function(){
-      if($('.feedback').size()>0){
-        require(['feedback'], function(fb){
-          fb.init($('.feedback'));
-        });
-      }
+  var doForAllPages = function(){
+    initCollectionsFilter();
+
+    if($('.eu-tooltip').size()>0){
+      require(['eu_tooltip'], function(euTooltip){
+        euTooltip.configure();
+      });
     }
-    
-    var doForAllPages = function(){
-      initCollectionsFilter();
+    initFeedback();
+  };
 
-      if($('.eu-tooltip').size()>0){
-        require(['eu_tooltip'], function(euTooltip){
-          euTooltip.configure();
-        });
-      }
-      initFeedback();
-    }
+  if(typeof pageName == 'undefined' || !pageName){
+    console.warn('pageName not specified - cannot bootstrap app');
+    return;
+  }
+  log('pageName ' + pageName);
 
-    if(typeof pageName == 'undefined' || !pageName){
-        console.warn('pageName not specified - cannot bootstrap app');
-        return;
-    }
-    console.log('pageName ' + pageName);
+  switch(pageName){
+    case 'explore/people':
+      promisedPageJS.resolve();
+      doForAllPages();
+      break;
 
-    switch(pageName){
-        case 'explore/people':
-            promisedPageJS.resolve();
-            doForAllPages();
-            break;
+    case 'explore/colours':
+      promisedPageJS.resolve();
+      doForAllPages();
+      break;
 
-        case 'explore/colours':
-            promisedPageJS.resolve();
-            doForAllPages();
-            break;
+    case 'explore/topics':
+      promisedPageJS.resolve();
+      doForAllPages();
+      break;
 
-        case 'explore/topics':
-            promisedPageJS.resolve();
-            doForAllPages();
-            break;
+    case 'explore/periods':
+      promisedPageJS.resolve();
+      doForAllPages();
+      break;
 
-        case 'explore/periods':
-            promisedPageJS.resolve();
-            doForAllPages();
-            break;
+    case 'explore/new_content':
+      promisedPageJS.resolve();
+      doForAllPages();
+      break;
 
-        case 'explore/new_content':
-            promisedPageJS.resolve();
-            doForAllPages();
-            break;
-
-        case 'explore/sources':
-            require(['util_foldable'], function(){
-              require(['util_filterable'], function(filter){
-                filter.init($('.eu-filter'),
-                  {
-                    selector:    '.eu-foldable > li',
-                    sel_title:   ' > h4 > a',
-                    children: {
-                      selector:  '.eu-foldable-data > li',
-                      sel_title: 'a',
-                      actions: {
-                        select: function($el){
-                          $el.closest('.eu-foldable-data').addClass('filter-force-show');
-                        },
-                        deselect: function($el){
-                          $el.closest('.eu-foldable-data').removeClass('filter-force-show');
-                        }
-                      }
-                    }
+    case 'explore/sources':
+      require(['util_foldable'], function(){
+        require(['util_filterable'], function(filter){
+          filter.init($('.eu-filter'),
+            {
+              selector:  '.eu-foldable > li',
+              sel_title:   ' > h4 > a',
+              children: {
+                selector:  '.eu-foldable-data > li',
+                sel_title: 'a',
+                actions: {
+                  select: function($el){
+                    $el.closest('.eu-foldable-data').addClass('filter-force-show');
+                  },
+                  deselect: function($el){
+                    $el.closest('.eu-foldable-data').removeClass('filter-force-show');
                   }
-                );
-              });
-            });
-            promisedPageJS.resolve();
+                }
+              }
+            }
+          );
+        });
+      });
+      promisedPageJS.resolve();
+      doForAllPages();
+      break;
+
+    case 'blog_posts/index':
+      require(['search_blog'], function(page){
+        page.initPage();
+        promisedPageJS.resolve(page);
+        doForAllPages();
+      });
+      break;
+    case 'blog_posts/show':
+      require(['search_blog'], function(page){
+        page.initPage();
+        promisedPageJS.resolve(page);
+        doForAllPages();
+      });
+      break;
+    case 'e7a_1418':
+      require(['e7a_1418'], function(page){
+        page.initPage();
+        promisedPageJS.resolve(page);
+        doForAllPages();
+      });
+      break;
+    case 'events/index':
+      require(['search_events'], function(page){
+        page.initPage();
+        promisedPageJS.resolve(page);
+        doForAllPages();
+      });
+      break;
+    case 'events/show':
+      require(['search_events'], function(page){
+        page.initPage();
+        promisedPageJS.resolve(page);
+        doForAllPages();
+      });
+      break;
+    case 'galleries/index':
+      require(['fashion_gallery_redirect'], function(fgr){
+        fgr.redirectOrCallback(function(){
+          require(['search_galleries'], function(page){
+            page.initPage();
+            promisedPageJS.resolve(page);
             doForAllPages();
-            break;
-
-        case 'blog_posts/index':
-            require(['search_blog'], function(page){
-                page.initPage();
-                promisedPageJS.resolve(page);
-                doForAllPages();
-            });
-            break;
-        case 'blog_posts/show':
-            require(['search_blog'], function(page){
-              page.initPage();
-              promisedPageJS.resolve(page);
-              doForAllPages();
-            });
-            break;
-        case 'e7a_1418':
-            require(['e7a_1418'], function(page){
-              page.initPage();
-              promisedPageJS.resolve(page);
-              doForAllPages();            	
-            });
-            break;
-        case 'events/index':
-            require(['search_events'], function(page){
-              page.initPage();
-              promisedPageJS.resolve(page);
-              doForAllPages();
-            });
-            break;
-        case 'events/show':
-            require(['search_events'], function(page){
-              page.initPage();
-              promisedPageJS.resolve(page);
-              doForAllPages();
-            });
-            break;
-        case 'galleries/index':
-            require(['fashion_gallery_redirect'], function(fgr){
-              fgr.redirectOrCallback(function(){
-                require(['search_galleries'], function(page){
-                  page.initPage();
-                  promisedPageJS.resolve(page);
-                  doForAllPages();
-                });
-              });
-            });
-            break;
-        case 'galleries/show':
-            require(['search_galleries'], function(page){
-              page.initPage();
-              promisedPageJS.resolve(page);
-              doForAllPages();
-            });
-            break;
-        case 'collections/show':
-            if((window.location.href.indexOf('?q=') == -1) && (window.location.href.indexOf('&q=') == -1)){
-              require(['fashion_redirect'], function(fr){
-                fr.redirectOrCallback(function(){
-                  require(['search_landing'], function(page){
-                    page.initPage(euSearchForm);
-                    promisedPageJS.resolve(page);
-                    doForAllPages();
-                  });
-                });
-              });
-            }
-            else{
-              require(['fashion_redirect'], function(fr){
-                fr.redirectOrCallback(function(){
-                  require(['search_results'], function(page){
-                    page.initPage(euSearchForm);
-                    promisedPageJS.resolve(page);
-                    doForAllPages();
-                  });
-                });
-              });
-            }
-            break;
-        case 'portal/show':
-            require(['search_object'], function(page){
-              page.initPage();
-              promisedPageJS.resolve(page);
-              doForAllPages();
-            });
-            break;
-        case 'portal/index':
-            var loadPageJS = function(){
-              require(['search_results'], function(page){
-                page.initPage(euSearchForm);
-                promisedPageJS.resolve(page);
-                doForAllPages();
-              });
-            }
-
-            if(typeof collectionName != 'undefined' && collectionName == 'fashion'){
-              require(['fashion_redirect'], function(fr){
-                fr.redirectOrCallback(function(){
-                  loadPageJS();
-                });
-              });
-            }
-            else{
-              loadPageJS();
-            }
-
-            break;
-
-        case 'pages/show':
-            promisedPageJS.resolve();
-            doForAllPages();
-            break;
-
-        case 'portal/static':
-            require(['util_foldable']);
-            promisedPageJS.resolve();
-            doForAllPages();
-            break;
-        case 'home/index':
-            require(['search_home'], function(page){
+          });
+        });
+      });
+      break;
+    case 'galleries/show':
+      require(['search_galleries'], function(page){
+        page.initPage();
+        promisedPageJS.resolve(page);
+        doForAllPages();
+      });
+      break;
+    case 'collections/show':
+      if((window.location.href.indexOf('?q=') == -1) && (window.location.href.indexOf('&q=') == -1)){
+        require(['fashion_redirect'], function(fr){
+          fr.redirectOrCallback(function(){
+            require(['search_landing', 'search_form'], function(page, euSearchForm){
               page.initPage(euSearchForm);
               promisedPageJS.resolve(page);
               doForAllPages();
             });
-            break;
-        case 'settings/language':
-            require(['settings'], function(page){
-              page.initPage();
+          });
+        });
+      }
+      else{
+        require(['fashion_redirect'], function(fr){
+          fr.redirectOrCallback(function(){
+            require(['search_results', 'search_form'], function(page, euSearchForm){
+              page.initPage(euSearchForm);
               promisedPageJS.resolve(page);
               doForAllPages();
             });
-            break;
-        default:
-            console.warn('pageName not recognised (' + pageName + ') - cannot bootstrap app');
-
-     }
-
-    return {
-      getPromisedPageJS: function(){
-        return promisedPageJS.promise();
+          });
+        });
       }
+      break;
+    case 'portal/show':
+      require(['search_object'], function(page){
+        page.initPage();
+        promisedPageJS.resolve(page);
+        doForAllPages();
+      });
+      break;
+    case 'portal/index':
+      var loadPageJS = function(){
+        require(['search_results', 'search_form'], function(page, euSearchForm){
+          page.initPage(euSearchForm, euSearchForm);
+          promisedPageJS.resolve(page);
+          doForAllPages();
+        });
+      };
+
+      if(typeof collectionName != 'undefined' && collectionName == 'fashion'){
+        require(['fashion_redirect'], function(fr){
+          fr.redirectOrCallback(function(){
+            loadPageJS();
+          });
+        });
+      }
+      else{
+        loadPageJS();
+      }
+
+      break;
+
+    case 'pages/show':
+      promisedPageJS.resolve();
+      doForAllPages();
+      break;
+
+    case 'portal/static':
+      require(['util_foldable']);
+      promisedPageJS.resolve();
+      doForAllPages();
+      break;
+    case 'home/index':
+      require(['search_home', 'search_form'], function(page, euSearchForm){
+        page.initPage(euSearchForm);
+        promisedPageJS.resolve(page);
+        doForAllPages();
+      });
+      break;
+    case 'settings/language':
+      require(['settings'], function(page){
+        page.initPage();
+        promisedPageJS.resolve(page);
+        doForAllPages();
+      });
+      break;
+    default:
+      console.warn('pageName not recognised (' + pageName + ') - cannot bootstrap app');
+  }
+
+  return {
+    getPromisedPageJS: function(){
+      return promisedPageJS.promise();
     }
+  };
 });
--- source/js/modules/eu/e7a_1418.js
+++ source/js/modules/eu/e7a_1418.js
@@ -1,25 +1,105 @@
-define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
+define(['jquery', 'purl'], function($) {
+
+  var e7aRoot = '';
+  var locale  = '';
+  var iframe  = $('iframe');
+
+  var pageData = {
+    'admin':{
+      'breadcrumbs': [
+        '.contributor'
+      ]
+    },
+    'admin/contributions':{
+      'breadcrumbs': [
+        '.contribution'
+      ]
+    },
+    'contributor': {
+      'breadcrumbs': [
+        '.contributor'
+      ]
+    },
+    'users/password/new': {
+      'breadcrumbs': [
+        '.contributor-url',
+        '.reset'
+      ]
+    },
+    'users/register': {
+      'breadcrumbs': [
+        '.contributor-url',
+        '.register'
+      ]
+    },
+    'users/sign-in' : {
+      'breadcrumbs': [
+        '.contributor-url',
+        '.sign-in'
+      ]
+    }
+  };
 
   function log(msg){
-    console.log(msg);  
+    console.log(msg);
   }
-  
+
+  function setBreadcrumbs(childUrl){
+
+    var fragment    = childUrl.replace(e7aRoot, '').replace(/\/[a-z][a-z]\//, '').split('?')[0];
+
+    log('fragment ' + fragment);
+
+    var breadcrumbs = pageData[fragment]['breadcrumbs'];
+
+    $('.breadcrumbs > .breadcrumb').addClass('js-hidden');
+
+    $.each(breadcrumbs, function(i, ob){
+      $('.breadcrumbs > .breadcrumb' + ob).removeClass('js-hidden');
+    });
+  }
+
+  function setSrc(){
+
+    var href = window.location.href;
+
+    if(href.indexOf('#') > -1){
+      var hash = window.location.href.split('#')[1];
+
+      if(hash.indexOf('=') > -1){
+        var fragment = hash.split('=')[1];
+        var url      = e7aRoot + '/' + locale + '/' + fragment;
+        iframe.attr('src', url);
+      }
+    }
+  }
+
+  function iframeUrlChange(e){
+
+    log(e.origin);
+    log(typeof e.data);
+    log('height:\t' + e.data.height);
+    log('child url:\t' + e.data.url);
+    setBreadcrumbs(e.data.url);
+    $('iframe.e7a1418').css('height', e.data.height + 'px');
+  }
+
   function initPage(){
-    log('INIT PAGE 14-18');
-    
-    window.addEventListener('message', function(e){
-      log(e.origin);
-      log(typeof e.data);
-      log('height:\t' + e.data.height);
-      log('url:\t' + e.data.url);
-    }, false);
-    
+
+    e7aRoot = iframe.data('base-url');
+    locale  = (window.location.href.match(/\/[a-z][a-z]\//) || '/en/').replace(/\//g, '');
+
+    log('Init 14-18 (root: ' + e7aRoot + ', locale: ' + locale + ')');
+
+    window.addEventListener('message', iframeUrlChange, false);
+    setSrc();
+
   }
-  
+
   return {
     initPage: function(){
       initPage();
     }
-  }
-  
+  };
+
 });
