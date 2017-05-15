65221c8d20f73bd6ffc372d305aa2a4fe512c54e
E.Y. Silva Navarrete <e.y.silvanavarrete@europeana.eu>
2017-05-15 15:13:43 +0200

    #2448 Dataset info form. Refactoring dataset-info js on ESLINT recommendation for styling and performance improvements

--- source/js/modules/eu/pandora/dataset-info.js
+++ source/js/modules/eu/pandora/dataset-info.js
@@ -1,62 +1,64 @@
 define(['jquery'], function ($) {
   'use strict';
 
-  function validateProfileForm() {
-    var role = $('form').data('role'),
-      mode = $('form').data('view-mode');
+  function handleForm() {
+    var form = $('form'),
+      role = form.data('role'),
+      mode = form.data('view-mode'),
+      editableBlockBtns = $('.editable-block-btns'),
+      previewBlockBtn = $('.preview-block-btn'),
+      edit = $('.edit'),
+      eu = $('.eu'),
+      prov = $('.prov'),
+      cancel = $('.cancel'),
+      euProv = $('.eu, .prov'),
+      submitBtn = $('.submit');
 
     if (mode === 'preview') {
-      $('.preview-block-btn').toggleClass('hidden');
+      previewBlockBtn.toggleClass('hidden');
     } else {
-      $('.editable-block-btns').toggleClass('hidden');
+      editableBlockBtns.toggleClass('hidden');
     }
 
-    $('.edit').click(function () {
+    edit.click(function () {
       $('form').attr('data-view-mode', 'update');
-      $('.preview-block-btn').toggleClass('hidden');
-      $('.editable-block-btns').toggleClass('hidden');
+      previewBlockBtn.toggleClass('hidden');
+      editableBlockBtns.toggleClass('hidden');
 
       if (role === 'europeana') {
-        $('.eu').attr('readonly', false);
-        $('.eu').attr('disabled', false);
+        eu.attr('readonly', false);
+        eu.attr('disabled', false);
       } else {
-        $('.prov').attr('readonly', false);
-        $('.prov').attr('disabled', false);
+        prov.attr('readonly', false);
+        prov.attr('disabled', false);
       }
     });
 
-    $('.cancel').click(function () {
-
-      if ($('.cancel').hasClass('preview') || $('.cancel').hasClass('update')) {
-        $('.eu, .prov').attr('readonly', true);
-        $('.eu, .prov').attr('disabled', true);
-        $('form').attr('data-view-mode', 'preview');
-        $('.cancel').toggleClass('update preview');
-        $('.preview-block-btn').toggleClass('hidden');
-        $('.editable-block-btns').toggleClass('hidden');
+    cancel.click(function () {
+      if (cancel.hasClass('preview') || cancel.hasClass('update')) {
+        euProv.attr('readonly', true);
+        euProv.attr('disabled', true);
+        form.attr('data-view-mode', 'preview');
+        cancel.toggleClass('update preview');
+        previewBlockBtn.toggleClass('hidden');
+        editableBlockBtns.toggleClass('hidden');
       } else {
-      //  TODO: on create mode after canceling the creation the user should be redirected to ?
+        //  TODO: on create mode after canceling the creation the user should be redirected to ?
         console.log('canceling dataset creation');
       }
     });
 
     // TODO : define the action to be taken after submitting changes.
-    $('.submit').click(function () {
+    submitBtn.click(function () {
       console.log('role ', role);
       console.log('view ', mode);
       console.log('submit form and take me to ...');
     });
   }
 
-  function formInit() {
-    validateProfileForm();
-    $('.edit').click(function () {
-    });
-  }
-
   return {
     formInit: function () {
-      formInit();
+      handleForm();
     }
   };
 });
