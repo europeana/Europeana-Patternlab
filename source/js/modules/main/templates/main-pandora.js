266e9a36371663a3e6d17740c5af9944a4b67155
E.Y. Silva Navarrete <e.y.silvanavarrete@europeana.eu>
2017-05-16 23:03:18 +0200

    #2505 Update design for login and register forms

--- source/js/modules/main/templates/main-pandora.js
+++ source/js/modules/main/templates/main-pandora.js
@@ -1,20 +1,22 @@
 require.config({
   paths: {
-    eu_tooltip:		'../../eu/tooltip/eu-tooltip',
-    jqDropdown:		'../../lib/jquery/jquery.dropdown',
-    jquery:			'../../lib/jquery/jquery',
-    pandoraPage:	'../../eu/pandora/pandora-page',
-    util_ellipsis:	'../../eu/util/ellipsis',
-    util_resize:	'../../eu/util/resize',
-    jush:			'../../lib/jush/jush',
-    smartmenus:		'../../lib/smartmenus/jquery.smartmenus',
-    smartmenus_keyboard:	'../../lib/smartmenus/keyboard/jquery.smartmenus.keyboard',
-    mustache:		'../../lib/mustache/mustache',
-    user_approval:  '../../eu/pandora/user-approval-form',
-    autocomplete:   '../../lib/autocomplete/autocomplete',
+    eu_tooltip: '../../eu/tooltip/eu-tooltip',
+    jqDropdown: '../../lib/jquery/jquery.dropdown',
+    jquery: '../../lib/jquery/jquery',
+    pandoraPage: '../../eu/pandora/pandora-page',
+    util_ellipsis: '../../eu/util/ellipsis',
+    util_resize: '../../eu/util/resize',
+    jush: '../../lib/jush/jush',
+    smartmenus: '../../lib/smartmenus/jquery.smartmenus',
+    smartmenus_keyboard: '../../lib/smartmenus/keyboard/jquery.smartmenus.keyboard',
+    mustache: '../../lib/mustache/mustache',
+    user_approval: '../../eu/pandora/user-approval-form',
+    autocomplete: '../../lib/autocomplete/autocomplete',
     // search filter selection files
-    search_form:                   '../../eu/search-form',
-    dataset_info_form: '../../eu/pandora/dataset-info'
+    search_form: '../../eu/search-form',
+    dataset_info_form: '../../eu/pandora/dataset-info',
+    register: '../../eu/pandora/register-form',
+    login: '../../eu/pandora/login-form'
   },
   'shim': {
     'smartmenus': ['jquery'],
@@ -23,10 +25,9 @@ require.config({
   waitSeconds: 200
 });
 
-
-require(['jquery', 'pandoraPage', 'dataset_info_form'], function ($, p, datasetForm) {
+require(['jquery', 'pandoraPage', 'dataset_info_form', 'register', 'login'], function ($, p, datasetForm, register, login) {
   p.pageInit();
   datasetForm.formInit();
+  register.formInit();
+  login.formInit();
 });
-
-
