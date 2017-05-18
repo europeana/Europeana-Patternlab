d855fc04cdaab05273d9a4b05d748056a82b4716
E.Y. Silva Navarrete <e.y.silvanavarrete@europeana.eu>
2017-05-18 21:49:27 +0200

    #2505 Update template and js for debugging validation on BE

--- source/js/modules/eu/pandora/login-form.js
+++ source/js/modules/eu/pandora/login-form.js
@@ -1,18 +1,18 @@
 define(['jquery'], function ($) {
   'use strict';
 
-  function handleForm() {
-    var submit = $('.login-btn'),
-      error_message = $('.error-message');
-
-    submit.click(function () {
-      error_message.toggleClass('hidden');
-    });
-  }
-
-  return {
-    formInit: function () {
-      handleForm();
-    }
-  };
+  // function handleForm() {
+  //   var submit = $('.login-btn'),
+  //     error_message = $('.error-message');
+  //
+  //   submit.click(function () {
+  //     error_message.toggleClass('hidden');
+  //   });
+  // }
+  //
+  // return {
+  //   formInit: function () {
+  //     handleForm();
+  //   }
+  // };
 });
