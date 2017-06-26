ae6a57203164464dbee0095fb5fb962f4c8751aa
andyjmaclean <andyjmaclean@gmail.com>
2017-06-26 13:10:20 +0200

    Re: #2216 Make google optimise parameter-driven

--- source/js/modules/main/templates/main-collections.js
+++ source/js/modules/main/templates/main-collections.js
@@ -182,13 +182,13 @@ require(['jquery'], function( $ ) {
             }
           }
 
-          if(typeof googleOptimise != 'undefined' && googleOptimise){
+          if(typeof googleOptimizeContainerID != 'undefined' && googleOptimizeContainerID){
             $('head').append('<style>.async-hide { opacity: 0 !important}</style>');
             (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
             h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
             (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-            })(window,document.documentElement, 'async-hide', 'dataLayer', 4000, {'GTM-K7MPPLG':true});
-            ga('require', 'GTM-K7MPPLG');
+            })(window,document.documentElement, 'async-hide', 'dataLayer', 4000, {googleOptimizeContainerID:true});
+            ga('require', googleOptimizeContainerID);
           }
 
           ga('send', 'pageview');
