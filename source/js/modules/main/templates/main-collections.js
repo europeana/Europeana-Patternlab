c8fea7c821c7f0c75ac2651b1235180b9e81627b
andyjmaclean <andyjmaclean@gmail.com>
2017-06-26 12:05:07 +0200

    Re: #2216 Make google optimise parameter-driven

--- source/js/modules/main/templates/main-collections.js
+++ source/js/modules/main/templates/main-collections.js
@@ -182,13 +182,15 @@ require(['jquery'], function( $ ) {
             }
           }
 
-          $('head').append('<style>.async-hide { opacity: 0 !important}</style>');
-          (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-          h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-          (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-          })(window,document.documentElement,'async-hide','dataLayer',4000,
-          {'GTM-K7MPPLG':true});
-          ga('require', 'GTM-K7MPPLG');
+          if(typeof googleOptimise != 'undefined' && googleOptimise){
+            $('head').append('<style>.async-hide { opacity: 0 !important}</style>');
+            (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+            h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+            (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+            })(window,document.documentElement, 'async-hide', 'dataLayer', 4000, {'GTM-K7MPPLG':true});
+            ga('require', 'GTM-K7MPPLG');
+          }
+
           ga('send', 'pageview');
         });
       });
