c3d139d35b698cf6ca77fbd8e474ea2430da813b
andyjmaclean <andyjmaclean@gmail.com>
2017-08-09 20:42:17 +0200

    Re: #2327 entity grid

--- source/js/modules/eu/search-entity.js
+++ source/js/modules/eu/search-entity.js
@@ -5,19 +5,15 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
   var totals        = [];
   var cmpTabs       = $('.eu-accordion-tabs');
   var accordionTabs = null;
+  var template      = null;
 
   function log(msg){
     console.log('Entity: ' + msg);
   }
 
   function showCarouselIfAvailable(ops){
-
-    log('TODO: pre-flight request to check if available...');
-
-    if(true){
-      $('.entity-related-outer').removeClass('js-hidden');
-      showCarousel(ops);
-    }
+    $('.entity-related-outer').removeClass('js-hidden');
+    showCarousel(ops);
   }
 
   function showCarousel(ops){
@@ -58,7 +54,7 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
         });
       });
 
-    }
+    };
 
     require(['eu_carousel', 'eu_carousel_appender'], function(Carousel, CarouselAppender){
       var appender = CarouselAppender.create({
@@ -74,7 +70,7 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
       ops.alwaysAfterLoad = addEllipsis;
       var carousel = Carousel.create(entitySimilar, appender, ops);
 
-      if(!ops.total_available || (ops.total_available > 0 && el.find('ul li').length == 0)){
+      if(!ops.total_available || (ops.total_available > 0 && $('.entity-related ul li').length == 0)){
         carousel.loadMore();
       }
     });
@@ -96,77 +92,74 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
     var getLoadParams = function(base, present){
 
       var params  = $.url(base).param();
-      var initial = 4;
-      var extra   = 2
+      var initial = 12;
+      var extra   = 12;
 
       params['per_page'] = present ? extra : initial;
-      params['page']     = (present / extra ) + 1;
+      params['page']     = Math.ceil(present / extra ) + 1;
 
       return base ? base.split('?')[0] + '?' + $.param(params) : '';
     };
 
-    var loadMoreItems = function(url, $tabContent, header, template, index){
+    var loadMoreItems = function(url, $tabContent, header, tabIndex){
 
-      var margin     = 220;
-      var heightL    = $tabContent.height() + margin;
-      var heightR    = $('.summary-column').height();
-      var spaceOnRow = $('.grid-sizer').width() * 2 <= $('.grid-sizer').parent().width() && $('.search-list-item').last().css('left') == '0px';
+      var items  = $tabContent.find('.results .result-items');
 
-      if(heightL < heightR || spaceOnRow){
-        var params = $.url(url).param();
-        var items  = $tabContent.find('.results .result-items');
-        //var total  = header.data('content-items-total');
+      url = getLoadParams(header.data('content-url'), $tabContent.find('.search-list-item').length);
 
-        url = getLoadParams(header.data('content-url'), $tabContent.find('.search-list-item').length);
+      header.addClass('loading');
 
-        header.addClass('loading');
+      loadMasonryItems(url, function(res){
 
-        loadMasonryItems(url, template, function(res){
+        //var rowSize   = getItemsPerRow();
+        var toAppend  = res.rendered;
 
-          rendered = $(res.rendered);
+        var dripFeed = function(index){
 
+          index = index + 1;
 
-          if($(rendered).find('img').length > 0){
+          if(hasSpaceToFill($tabContent)){
 
-            $(rendered).imagesLoaded(function(){
-              setTimeout(function(){
-                masonries[index].layout();
-              }, 200);
-            });
-            items.append(rendered);
-            masonries[index].appended(rendered);
+            if(index < toAppend.length){
+              appendItem(index);
+              return;
+            }
+            else{
+              if($tabContent.find('.search-list-item').length < totals[tabIndex]){
+                var newUrl = getLoadParams(header.data('content-url'), $tabContent.find('.results .result-items').length);
+                loadMoreItems(newUrl, $tabContent, header, tabIndex);
+                return;
+              }
+            }
           }
-          else{
-            items.append(rendered);
-            masonries[index].appended(rendered);
-            setTimeout(function(){
-              masonries[index].layout();
-            }, 200);
-          }
-
-          log('masonries[' + index + ']');
-          log('call masonry layout (load more)');
-
-          //    setTimeout(function(){
-          //    masonries[index].layout();
-          //  }, 1200);
-
           header.removeClass('loading');
-
-          if(items.find('.search-list-item').length >= totals[index]){
+          if(items.find('.search-list-item').length >= totals[tabIndex]){
             $tabContent.find('.show-more-mlt').addClass('js-hidden');
           }
-          else{
-            loadMoreItems(url, $tabContent, header, template, index)
-          }
+        };
 
-        });
-      }
-      else{
-        //setTimeout(function(){
-        //  masonries[index].layout();
-        //}, 200);
-      }
+        var appendItem = function(index){
+          var item   = $(toAppend[index]);
+          var hasImg = item.find('img').length > 0;
+
+          if(hasImg){
+            item.imagesLoaded(function(){
+              masonries[tabIndex].layout();
+              dripFeed(index);
+            });
+            items.append(item);
+            applyImgStyling(item.find('.item-image'));
+            masonries[tabIndex].appended(item);
+          }
+          else{
+            items.append(item);
+            masonries[tabIndex].appended(item);
+            masonries[tabIndex].layout();
+            dripFeed(index);
+          }
+        };
+        appendItem(0);
+      });
     };
 
     require(['eu_accordion_tabs'], function(euAccordionTabs){
@@ -177,25 +170,25 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
         cmpTabs,
         {
           'active': 0,
-          'fnOpenTab': function(index, $tabContent){
+          'fnOpenTab': function(tabIndex, $tabContent){
 
-            var header = $('.entity-main .tab-header:eq(' + index + ')');
+            var header = $('.entity-main .tab-header:eq(' + tabIndex + ')');
             if(header.hasClass('js-loaded')){
               log('call masonry layout (open tab)');
-              masonries[index].layout();
+              masonries[tabIndex].layout();
             }
             else {
 
-              var url      = getLoadParams(header.data('content-url'), $tabContent.find('.results .result-items').length);
-              var template = $('#js-template-entity-tab-content');
+              var url  = getLoadParams(header.data('content-url'), $tabContent.find('.results .result-items').length);
+              template = $('#js-template-entity-tab-content');
 
               header.addClass('loading');
 
-              loadMasonryItems(url, template, function(res){
+              loadMasonryItems(url, function(res){
                 $tabContent.find('.results').append(''
                   + '<ol class="result-items display-grid cf not-loaded">'
                   +   '<li class="grid-sizer"></li>'
-                  +   res.rendered
+                  +   res.rendered.join('')
                   + '</ol>'
                 );
                 cmpTabs.scrollTop(0);
@@ -209,10 +202,9 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
                   linkMore.text(linkMore.text().replace(/\(\)/, '(' + (res.total_formatted ? res.total_formatted : res.total ) + ')'));
                   linkMore.removeClass('js-hidden');
 
-                  totals[index] = res.total;
-                  //header.data('content-items-total', res.total);
+                  totals[tabIndex] = res.total;
 
-                  if($tabContent.find('.results .search-list-item').length >= totals[index]){
+                  if($tabContent.find('.results .search-list-item').length >= totals[tabIndex]){
                     $tabContent.find('.show-more-mlt').addClass('js-hidden');
                   }
 
@@ -220,13 +212,14 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
 
                 initMasonry('.eu-accordion-tabs .tab-content.active .result-items', function(){
                   header.removeClass('loading').addClass('js-loaded');
-                  loadMoreItems(url, $tabContent, header, template, index);
+                  if(hasSpaceToFill($tabContent)){
+                    loadMoreItems(url, $tabContent, header, tabIndex);
+                  }
                 });
               });
 
               require(['util_resize'], function(){
                 $(window).europeanaResize(function(){
-                  log('resize: fix height');
                   euAccordionTabs.fixTabContentHeight(cmpTabs);
                 });
               });
@@ -237,15 +230,27 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
     });
   }
 
-  function loadMasonryItems(url, template, callback){
+  function hasSpaceToFill($tabContent){
+    var margin = 220;
+    return $tabContent.height() + margin < $('.summary-column').height();
+  }
+
+  function applyImgStyling($item){
+    if($item.height() > 650){
+      $item.addClass('super-tall');
+    }
+  }
+
+  function loadMasonryItems(url, callback){
+
     require(['mustache', 'eu_accordion_tabs'], function(Mustache, euAccordionTabs){
       Mustache.tags = ['[[', ']]'];
 
       if(url && url.length > 0 && template.length > 0){
-        $.getJSON(url, null).done(function(data){
-          var rendered = '';
+        $.getJSON(url).done(function(data){
+          var rendered = [];
           $.each(data.search_results, function(i, ob){
-            rendered += '<li>' + Mustache.render(template.text(), ob) + '</li>';
+            rendered.push('<li>' + Mustache.render(template.text(), ob) + '</li>');
           });
           callback({
             rendered:        rendered,
@@ -268,45 +273,35 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
     require(['masonry', 'jqImagesLoaded'], function(Masonry){
 
       var masonry = new Masonry(cmpSel, {
-        itemSelector:    '.search-list-item',
-        columnWidth:     '.grid-sizer',
-        percentPosition: true
+        itemSelector:       '.search-list-item',
+        columnWidth:        '.grid-sizer',
+        percentPosition:    true,
+        horizontalOrder:    false,
+        transitionDuration: 0
       });
 
-      log('made masonry');
       masonries.push(masonry);
 
       $cmp.on('layoutComplete', function(){
-        log('layout complete: new handler / 400');
         setTimeout(function(){
-          log('400 up');
           accordionTabs.fixTabContentHeight(cmpTabs);
         }, 400);
       });
 
       $cmp.imagesLoaded().progress(function(){
         if(masonry){
-          log('call masonry layout (progress)');
           masonry.layout();
         }
       }).done(function(){
-
-        var hasSuperTall = false;
         $('.item-image').each(function(i, ob){
-          var $ob = $(ob);
-          if($ob.height() > 650){
-            hasSuperTall = true;
-            $ob.addClass('super-tall');
-          }
+          applyImgStyling($(ob));
         });
-
         log('call masonry layout (done)');
         masonry.layout();
         if(callback){
           callback();
         }
       });
-//      masonries.push(masonry);
     });
   }
 
@@ -322,7 +317,7 @@ define(['jquery', 'util_scrollEvents', 'purl'], function($, scrollEvents) {
                 'top': offset,
                 'margin-bottom': offset
               });
-            }
+            };
             $(window).europeanaResize(function(){
               thumblessLayout();
             });
