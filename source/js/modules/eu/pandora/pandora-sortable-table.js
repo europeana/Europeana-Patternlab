663a60ce9e3b98cea933bc25ff5f3b1625bf7d2e
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2017-08-10 12:12:54 +0200

    sortable table
    added sorting on date, sorting desc and asc

--- source/js/modules/eu/pandora/pandora-sortable-table.js
+++ source/js/modules/eu/pandora/pandora-sortable-table.js
@@ -8,31 +8,67 @@ define(['jquery'], function ($) {
       
     if ($('.table-sortable').length === 0) return false;
 
-    $('.table-sortable').find('th').each (function(i, h) {
-      $(h).on( 'click', function() {
-        sortColumn(i);
+    $('.table-sortable').each (function(ti, t) {
+      $(t).find('th').each (function(i, h) {
+        $(h).on( 'click', function() {
+          if (!$(h).data('sorting-order') || $(h).data('sorting-order') === 'asc') {
+            $(h).data('sorting-order', 'desc');
+          } else {
+            $(h).data('sorting-order', 'asc');
+          }
+          sortColumn(i, $(t), $(this).hasClass('sort-date'), $(h).data('sorting-order'));
+        });
       });
     });
 
   }
 
-  function sortColumn (clicked) {
+  function sortColumn (clicked, table, isDate, sortingOrder) {
 
     var sorting = true;
 
     while(sorting) {
-      var tableRows = $('.table-sortable').find('tr').not(':first');
+      var tableRows = table.find('tr').not(':first');
       for(var i=0; i<tableRows.length-1;i++) {
-        if ($(tableRows[i]).find('td').eq(clicked).text().toLowerCase() > $(tableRows[i+1]).find('td').eq(clicked).text().toLowerCase()) {
-          $(tableRows[i+1]).insertBefore($(tableRows[i]));
-          sorting = true;
-          break;
-        } else {
-          sorting = false;
+        
+        var value1 = $(tableRows[i]).find('td').eq(clicked).text().toLowerCase();
+        var value2 = $(tableRows[i+1]).find('td').eq(clicked).text().toLowerCase();
+
+        if (isDate === true) {
+          value1 = convertDateToTimestamp($(tableRows[i]).find('td').eq(clicked).text());
+          value2 = convertDateToTimestamp($(tableRows[i+1]).find('td').eq(clicked).text());
         }
+
+        if (sortingOrder === 'desc') {
+
+          if (value1 > value2) {
+            $(tableRows[i+1]).insertBefore($(tableRows[i]));
+            sorting = true;
+            break;
+          } else {
+            sorting = false;
+          }
+
+        } else if (sortingOrder === 'asc') {
+
+          if (value2 > value1) {
+            $(tableRows[i+1]).insertBefore($(tableRows[i]));
+            sorting = true;
+            break;
+          } else {
+            sorting = false;
+          }
+
+        }
+
       }    
     }
 
+  }
+
+  function convertDateToTimestamp (d) {
+    var s = d.match(new RegExp(/(\d{1,2})[\/](\d{1,2})[\/](\d{4}) [-] (\d{1,2})[:](\d{1,2})/));
+    return new Date(s[3], s[2], s[1], s[4], s[5]).getTime();
   }
 
   return {
