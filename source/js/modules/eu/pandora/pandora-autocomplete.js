555ab003af5b5f32a2a8de5c2fe8b375827e554b
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2017-09-06 11:31:50 +0200

    javascript linting

--- source/js/modules/eu/pandora/pandora-autocomplete.js
+++ source/js/modules/eu/pandora/pandora-autocomplete.js
@@ -4,10 +4,9 @@ define(['jquery'], function ($) {
 
     if ($('.metis-autocomplete').length === 0) { return false; }
 
-      $('.metis-autocomplete').each(function(i, ac) {   
+    $('.metis-autocomplete').each(function(i, ac) {   
 
       var orgInput = $(ac).find('.metis-autocomplete-wrap input');
-
       if(orgInput.length > 0 && orgInput.data('autocomplete-url').length > 0) {
 
         // add autocomplete
@@ -24,63 +23,61 @@ define(['jquery'], function ($) {
         });
       }
     });
-
   }
 
   function addAutocomplete(options){
-    require(['eu_autocomplete', 'util_resize'], function(Autocomplete){
+    require(['eu_autocomplete', 'util_resize'], function(Autocomplete) {
+      var selInput = options.selInput;
+      var selectedItems = options.autoCompleteEl.next('.metis-autocomplete-selected');
 
-    var selInput = options.selInput;
-    var selectedItems = options.autoCompleteEl.next('.metis-autocomplete-selected');
+      Autocomplete.init({
+        evtResize       : 'europeanaResize',
+        fnOnSelect     : function(sel) {
 
-    Autocomplete.init({
-      evtResize       : 'europeanaResize',
-      fnOnSelect     : function(sel) {
+        var val   = sel.data('term');
+        var itemId = sel.data('id');
 
-      var val   = sel.data('term');
-      var itemId = sel.data('id');
+        // clear input
+        $(selInput).val('');
 
-      // clear input
-      $(selInput).val('');
-
-      // item already selected
-      if($(selectedItems).find('li [value="' + itemId + '"]').length > 0) {
-        return;
-      }
-
-      // create a new tag
-      var tagnr = $(selectedItems).find('.metis-autocomplete-selected-wrap .tag').length;
-      var tag = $('<li class="tag"><input type="hidden" name="' + $(selInput).attr('name') + '[' + tagnr +  '].id" value="' + itemId + '">' + val + '</li>')
-        .appendTo($(selectedItems).find('.metis-autocomplete-selected-wrap'));
-        $('<svg class="icon icon-delete"><use xlink:href="#icon-delete"/></svg>')
-        .appendTo(tag)
-        .on('click', function(){
-          $(this).closest('.tag').remove();
-        });
-
-      },
-      fnPreProcess     : function(term, data) {
-        var escapeRegExp = function(str){
-          return str.replace(/[\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-        };
-        var re = new RegExp('\\b' + escapeRegExp(term), 'i');
-        for(var i=0; i<data.length; i++){
-          var val     = data[i].text;
-          var match   = val.match(re);
-          data[i].textPreMatch  = val.substr(0, val.indexOf(match));
-          data[i].textPostMatch = val.substr(val.indexOf(match) + (match+'').length);
-          data[i].textMatch     = match;
+        // item already selected
+        if($(selectedItems).find('li [value="' + itemId + '"]').length > 0) {
+          return;
         }
-        return data;
-      },
-      hideOnSelect     : true,
-      itemTemplateText : '<li data-term="[[text]]" data-id="[[item-id]]"><span>[[textPreMatch]]<span class="match"><b>[[textMatch]]</b></span>[[textPostMatch]]</span></li>',
-      minTermLength    : options.min_chars ? options.min_chars : 3,
-      paramName        : 'text',
-      selInput         : selInput,
-      submitOnEnter    : true,
-      url              : options.url
-      });
+
+        // create a new tag
+        var tagnr = $(selectedItems).find('.metis-autocomplete-selected-wrap .tag').length;
+        var tag = $('<li class="tag"><input type="hidden" name="' + $(selInput).attr('name') + '[' + tagnr +  '].id" value="' + itemId + '">' + val + '</li>')
+          .appendTo($(selectedItems).find('.metis-autocomplete-selected-wrap'));
+          $('<svg class="icon icon-delete"><use xlink:href="#icon-delete"/></svg>')
+          .appendTo(tag)
+          .on('click', function(){
+            $(this).closest('.tag').remove();
+          });
+
+        },
+        fnPreProcess     : function(term, data) {
+          var escapeRegExp = function(str){
+            return str.replace(/[\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+          };
+          var re = new RegExp('\\b' + escapeRegExp(term), 'i');
+          for(var i=0; i<data.length; i++){
+            var val     = data[i].text;
+            var match   = val.match(re);
+            data[i].textPreMatch  = val.substr(0, val.indexOf(match));
+            data[i].textPostMatch = val.substr(val.indexOf(match) + (match+'').length);
+            data[i].textMatch     = match;
+          }
+          return data;
+        },
+        hideOnSelect     : true,
+        itemTemplateText : '<li data-term="[[text]]" data-id="[[item-id]]"><span>[[textPreMatch]]<span class="match"><b>[[textMatch]]</b></span>[[textPostMatch]]</span></li>',
+        minTermLength    : options.min_chars ? options.min_chars : 3,
+        paramName        : 'text',
+        selInput         : selInput,
+        submitOnEnter    : true,
+        url              : options.url
+        });
     });
   }  
 
