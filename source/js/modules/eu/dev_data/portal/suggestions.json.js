5a6871b9af4612e2918d3a714790c87cb22d713d
andyjmaclean <andyjmaclean@gmail.com>
2017-09-15 14:16:35 +0200

    Re: #2416 slideable content

--- source/js/modules/eu/dev_data/portal/suggestions.json.js
+++ source/js/modules/eu/dev_data/portal/suggestions.json.js
@@ -3,148 +3,145 @@ define([], function(){
   var suggestions = {
     'tab_items': [
       {
-        'tab_subtitle':       '273 Results',
-        'more_results_label': 'View all 273 results on DPLA',
-        'more_results_url':   'javascript:alert(\'Link to more DPLA results\')',
-        'search_results': [
+        'tab_subtitle': '273 Results',
+        'items': [
           {
             'title': 'The Lighthouse, Glasgow (The Herald Building) - Exterior',
-            'is_video': true,
             'url': "javascript:alert('follow external url...');"
           },
-
           {
             'title': 'Glasgow School of Art - Exterior, Renfrew Street metalwork | Mackintosh, Charles Rennie',
-            'is_video': true,
             'img': {
-              'src': '/images/search/search-result-thumb-lincoln.jpg',
-              'alt': 'Rectangle'
+              'src': '/images/search/search-result-thumb-lincoln.jpg'
             },
             'url': "javascript:alert('follow external url...');"
           },
-
           {
             'title': 'Glasgow',
-            'is_image': true,
             'img': {
-              'src': '/images/search/search-result-thumb-3.jpg',
-              'alt': 'Rectangle'
+              'src': '/images/search/search-result-thumb-3.jpg'
             },
             'url': "javascript:alert('follow external url...');"
           },
-
           {
             'title': 'The Lighthouse, Glasgow (Glasgow Herald Building) - Model (on display) | Mackintosh, Charles Rennie',
-            'url': "javascript:alert('follow external url...');",
-            'is_image': true,
+            'url': "javascript:alert('follow external url...');"
+          },
+          {
+            'title': 'The Lighthouse (2), Glasgow (The Herald Building) - Exterior',
+            'url': "javascript:alert('follow external url...');"
+          },
+          {
+            'title': 'Glasgow School of Art - Exterior (2), Renfrew Street metalwork | Mackintosh, Charles Rennie',
+            'is_video': true,
+            'img': {
+              'src': '/images/search/search-result-thumb-lincoln.jpg'
+            },
+            'url': "javascript:alert('follow external url...');"
+          },
+          {
+            'title': 'Glasgow (2)',
+            'img': {
+              'src': '/images/search/search-result-thumb-3.jpg'
+            },
+            'url': "javascript:alert('follow external url...');"
+          },
+          {
+            'title': 'The Lighthouse, Glasgow (Glasgow Herald Building) (2) - Model (on display) | Mackintosh, Charles Rennie',
+            'url': "javascript:alert('follow external url...');"
           }
         ]
       },
       {
-        'tab_subtitle':       '612 Results',
-        'more_results_label': 'View all 612 results on Internet Archive',
-        'more_results_url':   'javascript:alert(\'Link to more Internet Archive results\')',
-        'search_results': [
+        'tab_subtitle': '612 Results',
+        'items': [
           {
             'title': 'Glasgow School of Art - Interior, lighting (rose motif) | Mackintosh, Charles Rennie',
-            'is_image': true,
+            'external': true,
             'img': {
-              'src': '/images/search/search-result-thumb-5.jpg',
-              'alt': 'Rectangle'
+              'src': '/images/search/search-result-thumb-5.jpg'
             },
             'url': "javascript:alert('follow external url...');"
           },
           {
             'title': 'Glasgow School of Art - Interior, alcove for mosaic | Mackintosh, Charles Rennie',
-            'is_image': true,
+            'external': true,
             'img': {
-              'src': '/images/search/search-result-thumb-6.jpg',
-              'alt': 'Rectangle'
+              'src': '/images/search/search-result-thumb-6.jpg'
             },
             'url': "javascript:alert('follow external url...');"
           },
           {
             'title': 'School of Art - Glasgow',
+            'external': true,
             'url': "javascript:alert('follow external url...');"
           },
           {
             'title': 'Militia Company of District II under the Command of Captain Frans Banninck Cocq, Known as de Night Watch',
-            'is_audio': true,
+            'external': true,
             'url': "javascript:alert('follow external url...');",
           }
         ]
       },
       {
-        'tab_subtitle':       '189 Results',
-        'more_results_label': 'View all 189 results on DigitalNZ',
-        'more_results_url':   'javascript:alert(\'Link to more DigitalNZ results\')',
-        'search_results': [
+        'tab_subtitle': '189 Results',
+        'items': [
           {
             'title': 'The Doorway with some steps.',
-            'is_audio': true,
+            'external': true,
             'img': {
-              'src': '/images/search/search-result-thumb-1.jpg',
-              'alt': 'Rectangle'
+              'src': '/images/search/search-result-thumb-1.jpg'
             },
             'url': "javascript:alert('follow external url...');"
           },
           {
             'title': 'Self portrait',
+            'external': true,
             'url': "javascript:alert('follow external url...');"
           },
           {
             'title': 'Belshazzars Feast',
-            'is_text': true,
+            'external': true,
             'img': {
-              'src': 'http://placeimg.com/400/300/nature',
-              'alt': 'Rectangle'
+              'src': 'http://placeimg.com/400/300/nature'
             },
             'url': "javascript:alert('follow external url...');"
           },
           {
             'title': 'The Stoning of Saint Stephen',
-            'is_image': true,
+            'external': true,
             'url': "javascript:alert('follow external url...');"
           }
         ]
       },
       {
-        'tab_subtitle':       '5 Results',
-        'more_results_label': 'View all 5 results on Canadiana',
-        'more_results_url':   'javascript:alert(\'Link to more Canadiana results\')',
-        'search_results': [
+        'tab_subtitle': '5 Results',
+        'items': [
           {
             'title': 'The Anatomy Lesson of Dr. Nicolaes Tulp',
-            'is_audio': true,
+            'external': true,
             'img': {
-              'src': '/images/search/search-result-thumb-4.jpg',
-              'alt': 'Rectangle'
+              'src': '/images/search/search-result-thumb-4.jpg'
             },
             'url': "javascript:alert('follow external url...');"
           },
           {
             'title': 'An Elephant, a photograph, a drawing, a painting and a title that is long enough to line-wrap',
-            'is_image': true,
+            'external': true,
             'img': {
-              'src': '/images/search/search-result-thumb-giraffe.png',
-              'alt': 'Rectangle'
+              'src': '/images/search/search-result-thumb-giraffe.png'
             },
             'url': "javascript:alert('follow external url...');"
           },
           {
             'title': 'Feast - Rembrandt',
-            'url': "javascript:alert('follow external url...');",
+            'external': true,
+            'url': "javascript:alert('follow external url...');"
           },
           {
             'url': "javascript:alert('follow external url...');",
             'title': 'The Stoning of Saint Stephen | Rembrandt van Rijn',
-            'is_text': true,
-            'text': {
-              'medium': 'The Stoning of Saint Stephen is the first painting by artist Rembrandt.'
-            },
-            'year': {
-              'long': '1625'
-            }
+            'external': true
           }
       ]
     }]
@@ -157,7 +154,6 @@ define([], function(){
 
       if(params.from == 'vermeer'){
         data = suggestions.tab_items[0];
-        //data.search_results = [];
       }
       else if(params.from == 'rijks'){
         data = suggestions.tab_items[1];
