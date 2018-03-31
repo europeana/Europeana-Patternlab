c8f612d31840055b7574d750223e46215a246b8b
andyjmaclean <andyjmaclean@gmail.com>
2018-03-31 17:37:44 +0200

    hound fix

--- source/js/modules/eu/exhibitions.js
+++ source/js/modules/eu/exhibitions.js
@@ -523,8 +523,7 @@ define(['jquery', 'util_resize', 'purl', 'jqScrollto'], function ($) {
                 duration:       introDuration / 1.1,
                 reverse:        true
               }
-            )
-            .addTo(smCtrl).setTween(
+            ).addTo(smCtrl).setTween(
               TweenMax.fromTo(
                 fullDescription,
                 1,
