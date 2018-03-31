7c232332c33b4f5c7610349fab41908b72fa5657
andyjmaclean <andyjmaclean@gmail.com>
2018-03-31 18:00:47 +0200

    hound conf tweak

--- source/js/modules/eu/exhibitions.js
+++ source/js/modules/eu/exhibitions.js
@@ -821,16 +821,16 @@ define(['jquery', 'util_resize', 'purl', 'jqScrollto'], function ($) {
             triggerHook:    'onEnter'
           }
         )
-          .addTo(smCtrl)
-          .setTween(TweenMax.to('.ve-progress-nav', 1, {'right': '-1em', ease: window.Cubic.easeOut}))
-          .on('enter', function(){
-            progNavActive = false;
-            $('.slide-nav-next:first').hide();
-          })
-          .on('leave', function(){
-            progNavActive = true;
-            $('.slide-nav-next:first').show();
-          });
+        .addTo(smCtrl)
+        .setTween(TweenMax.to('.ve-progress-nav', 1, {'right': '-1em', ease: window.Cubic.easeOut}))
+        .on('enter', function(){
+          progNavActive = false;
+          $('.slide-nav-next:first').hide();
+        })
+        .on('leave', function(){
+          progNavActive = true;
+          $('.slide-nav-next:first').show();
+        });
 
         $('.ve-progress-nav a').on('click', function(e){
           e.preventDefault();
