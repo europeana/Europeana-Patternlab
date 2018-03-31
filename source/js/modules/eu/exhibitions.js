50a770439f7c57b2d8ad7e6a4c9e74697ebfce1c
andyjmaclean <andyjmaclean@gmail.com>
2018-03-31 17:07:48 +0200

    hound fix

--- source/js/modules/eu/exhibitions.js
+++ source/js/modules/eu/exhibitions.js
@@ -524,21 +524,21 @@ define(['jquery', 'util_resize', 'purl', 'jqScrollto'], function ($) {
                 reverse:        true
               }
             )
-              .addTo(smCtrl)
-                .setTween(
-                  TweenMax.fromTo(
-                    fullDescription,
-                    1,
-                    {
-                      top:    introHeight,
-                    },
-                    {
-                      top:    (introDuration / 1.1) + (introHeight-fullDescription.height()) / 2,
-                      ease:   window.Cubic.easeIn
-                    }
-                  )
-                )
-              );
+            .addTo(smCtrl)
+            .setTween(
+              TweenMax.fromTo(
+                fullDescription,
+                1,
+                {
+                  top:    introHeight,
+                },
+                {
+                  top:    (introDuration / 1.1) + (introHeight-fullDescription.height()) / 2,
+                  ease:   window.Cubic.easeIn
+                }
+              )
+            )
+          );
         }
         else{
           console.log('first slide is not an intro!');
