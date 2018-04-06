cb4ba5767adec620c8dcde1c35e06aa3c09edea3
andyjmaclean <andyjmaclean@gmail.com>
2018-04-06 22:49:15 +0200

    Remove unused files / added resize unit test

---
+++ source/js/unit-tests/eu/util/eu-resize-test.js
@@ -0,0 +1,31 @@
+define(['util_resize', 'jasmine_jquery'], function(Ellipsis){
+  'use strict';
+
+  describe('Eu Resize', function(){
+
+    it('Debounces the window resize event', function(done){
+
+      var callsToIssue       = 5;
+      var resizeCallsMade    = 0;
+      var debouncedCallsMade = 0;
+
+      $(window).on('resize', function(){
+        resizeCallsMade += 1;
+      });
+
+      $(window).europeanaResize(function(){
+        debouncedCallsMade += 1;
+      });
+
+      for(var i=0; i<callsToIssue; i++){
+        $(window).resize();
+      }
+
+      setTimeout(function(){
+        expect(resizeCallsMade).toEqual(callsToIssue);
+        expect(debouncedCallsMade).toEqual(1);
+        done();
+      }, 100);
+    });
+  });
+});
