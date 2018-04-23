50349fce1d1c3ebad2d2a79f39650d3c8b8c04a1
Andy MacLean <andyjmaclean@gmail.com>
2018-04-23 13:27:09 +0200

    EC-2432 Derive transcription coordinate divide factor

--- source/js/modules/eu/leaflet/eu-leaflet-iiif.js
+++ source/js/modules/eu/leaflet/eu-leaflet-iiif.js
@@ -2,12 +2,22 @@ require(['leaflet_iiif'], function(){
 
   var L = window.L;
 
+  var _this;
+
   L.TileLayer.Iiif.Eu = L.TileLayer.Iiif.extend({
     _fitBounds: function(force) {
       if(!force && typeof window.blockIiifFitBounds !== 'undefined'){
         return;
       }
       L.TileLayer.Iiif.prototype._fitBounds.call(this);
+    },
+    onAdd: function(map){
+      _this = this;
+      $.when(_this._infoDeferred).done(function() {
+        _this.divideFactor = _this.x / _this._imageSizes[0].x;
+        alert('divideFactor = ' + _this.divideFactor);
+      });
+      L.TileLayer.Iiif.prototype.onAdd.call(this, map);
     }
   });
 
