a6e34a431ef128bea4476b67826c753d185d6aa9
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2018-07-31 14:31:52 +0200

    feat/EC-2935-alphabetbar

--- source/js/modules/eu/channels/channels-browse.js
+++ source/js/modules/eu/channels/channels-browse.js
@@ -18,6 +18,15 @@ define(['jquery'], function($){
         titleBar.test();
       });
 
+      anchorList.find('a').each(function() {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+          }, 1000);
+        });
+      });
+
     });
   }
 
