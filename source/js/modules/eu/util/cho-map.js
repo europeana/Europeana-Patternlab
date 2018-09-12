32b2e51fc93d04e652df660432291dde676a7372
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2018-09-12 14:45:45 +0200

    bug/EC-2968-update temlate and javascript locations

--- source/js/modules/eu/util/cho-map.js
+++ source/js/modules/eu/util/cho-map.js
@@ -7,12 +7,11 @@ define(['jquery', 'leaflet'], function($, L){
     var mapInfoId = 'map-info';
     var osmUrl    = location.protocol + '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-    $('.map').after('<div id="' + mapInfoId + '"></div>');
+    $('.map-data-header').after('<div id="' + mapInfoId + '"></div>');
 
     var osmAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 
     var map = L.map($('.map')[0], {
-      center : new L.LatLng($(markers[0]).data('latitude'), $(markers[0]).data('longitude')),
       zoomControl : true,
       zoomsliderControl: false,
       zoom : 8
@@ -34,21 +33,28 @@ define(['jquery', 'leaflet'], function($, L){
     var pairs        = [];
 
     for(var i = 0; i < markers.length; i++){
-      var pair = [$(markers[i]).data('latitude'), $(markers[i]).data('longitude')];
-      pairs.push(pair);
-      L.marker(pair);
-      L.marker(pair).addTo(map);
+      if ($(markers[i]).data('latitude') && $(markers[i]).data('longitude')) {
+        var pair = [$(markers[i]).data('latitude'), $(markers[i]).data('longitude')];
+        pairs.push(pair);
+        L.marker(pair);
+        L.marker(pair).addTo(map);
+      }
 
       coordLabels += $(markers[i]).data('label') + ' ';
-      coordLabels += $(markers[i]).data('latitude') + '&deg; ' + ($(markers[i]).data('latitude') > 0 ? labels.n : labels.s) + ', ';
-      coordLabels += $(markers[i]).data('longitude') + '&deg; ' + ($(markers[i]).data('longitude') > 0 ? labels.e : labels.w);
+      if ($(markers[i]).data('latitude') && $(markers[i]).data('longitude')) {
+        coordLabels += $(markers[i]).data('latitude') + '&deg; ' + ($(markers[i]).data('latitude') > 0 ? labels.n : labels.s) + ', ';
+        coordLabels += $(markers[i]).data('longitude') + '&deg; ' + ($(markers[i]).data('longitude') > 0 ? labels.e : labels.w);
+      } 
       coordLabels += '<br/>';
     }
 
     if(pairs.length > 0){
+      map.setView(pairs[0]);
       map.fitBounds(pairs, {padding: [50, 50]});
+    } else {
+      $('.map').remove();
     }
-
+ 
     $('#' + mapInfoId).html(coordLabels);
 
     $.each(
