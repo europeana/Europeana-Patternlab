0f0016f8c57c966f35e0f2d9eb72ecf9917cd6a5
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2018-09-13 16:24:29 +0200

    bug/EC-2968-update-unittests

--- source/js/unit-tests/eu/util/cho-map-test.js
+++ source/js/unit-tests/eu/util/cho-map-test.js
@@ -2,7 +2,6 @@ define(['util_cho_map', 'jasmine_jquery'], function(MapUtil){
   'use strict';
 
   var basePath       = 'base/js/unit-tests/fixtures/util';
-  var mapData;
   var markers;
 
   describe('Map Util', function(){
@@ -10,21 +9,17 @@ define(['util_cho_map', 'jasmine_jquery'], function(MapUtil){
     beforeEach(function(){
       jasmine.getFixtures().fixturesPath = basePath;
       window.loadFixtures('fx-cho-map.html');
-      mapData = {
-        'labels':{'n':'North','s':'South','e':'East','w':'West'}
-      };
-
       markers = $('.markers a');
     });
 
     it('expects at least one valid pair of coordinates', function(){
       markers = [];
-      var res = MapUtil.loadMap(mapData, markers);
+      var res = MapUtil.loadMap(markers);
       expect(res).not.toBe(true);
     });
 
     it('displays a map', function(){
-      var res = MapUtil.loadMap(mapData, markers);
+      var res = MapUtil.loadMap(markers);
       expect(res).toBe(true);
     });
 
