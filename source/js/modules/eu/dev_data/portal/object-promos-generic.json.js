33b9e66ed764702662311cb1ac77803fb3abd150
Andy MacLean <andyjmaclean@gmail.com>
2018-09-20 11:52:21 +0200

    EC-3037 normalise model for icons

--- source/js/modules/eu/dev_data/portal/object-promos-generic.json.js
+++ source/js/modules/eu/dev_data/portal/object-promos-generic.json.js
@@ -50,6 +50,7 @@ define([], function(){
         "short": "Text but no title."
       },
       "tags": false,
+      "media_type": "3d",
       "relation": "All data fields accepted in generic card apart from icons (will show but may collide with text)"
     }
   ];
