461d98df7cd094ff6e5654b0ca9f2c7ddd0ae00d
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2018-11-15 16:23:33 +0100

    feat/EC-3111-simplify model

--- source/js/modules/eu/dev_data/portal/object-promos-exhibition.json.js
+++ source/js/modules/eu/dev_data/portal/object-promos-exhibition.json.js
@@ -1,58 +1,14 @@
 define([], function(){
 
   var promo = {
-    "states": {
-      "items": [
-        {
-          "image": {
-            "thumbnail": {
-              "url": "/images/virtual-exhibitions/art-nouveau/wiki-art-nouveau-thumbnail.png"
-            }
-          }
-        },
-        {
-          "body": "<p>Tired of the old conventions, Art Nouveau artists readily embraced glass in their projects. It became an essential tool in creating luminous living areas and spaces</p>",
-          "image": {
-            "thumbnail": {
-              "url": "/images/virtual-exhibitions/art-nouveau/sagrada-familia-thumbnail.png"
-            }
-          }
-        },
-        {
-          "logo": {
-            "thumbnail": {
-              "url": "../../images/europeana-logo-collections.svg"
-            }
-          },
-          "image": {
-            "thumbnail":{
-              "url": "/images/virtual-exhibitions/art-nouveau/stairs-thumbnail.png"
-            }
-          }
-        }
-      ]
-    },
-    "excerpt": false,
+    "exhibtion_card": true,
     "url": "javascript:alert('open this exhibition (1)')",
-    "icon": "multi-page",
     "title": "The Exhibition: Johann Vermeer can be found here too along with other objets d'art including some Art Nouveau",
-    "relation": "Feautues Johann Vermeer",
-    "tags": {
-      "items": [
-        {
-          "url": "javascript:alert('open tag')",
-          "text": "exhibition"
-        },
-        {
-          "url": "javascript:alert('open tag')",
-          "text": "vermeer"
-        },
-        {
-          "url": "javascript:alert('open tag')",
-          "text": "art"
-        }
-      ]
-    }
+    "description": "<p>Tired of the old conventions, Art Nouveau artists readily embraced glass in their projects. It became an essential tool in creating luminous living areas and spaces</p>",
+    "image": "/images/virtual-exhibitions/art-nouveau/wiki-art-nouveau-thumbnail.png",
+    "logo_url": "../../images/europeana-logo-collections.svg",
+    "type": "Exhibition",
+    "relation": "Feautues Johann Vermeer"
   };
 
   return {
