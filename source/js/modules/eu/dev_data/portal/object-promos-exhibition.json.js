81a6f7df0b1808cb73af552f8931db2b043a9583
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2018-11-26 15:54:58 +0100

    feat/EC-3111 update icons for firefox, update foyer

--- source/js/modules/eu/dev_data/portal/object-promos-exhibition.json.js
+++ source/js/modules/eu/dev_data/portal/object-promos-exhibition.json.js
@@ -4,7 +4,7 @@ define([], function(){
     "url": "javascript:alert('open this exhibition (1)')",
     "title": "The Exhibition: Johann Vermeer can be found here too along with other objets d'art including some Art Nouveau",
     "description": "<p>Tired of the old conventions, Art Nouveau artists readily embraced glass in their projects. It became an essential tool in creating luminous living areas and spaces</p>",
-    "image": "/images/virtual-exhibitions/art-nouveau/wiki-art-nouveau-thumbnail.png",
+    "images": ["/images/virtual-exhibitions/art-nouveau/wiki-art-nouveau-thumbnail.png"],
     "logo_url": "../../images/europeana-logo-collections.svg",
     "type": "Exhibition",
     "relation": "Feautues Johann Vermeer"
