7b8359a731665c8ad45b14cef04fa28407cf2fd8
Andy MacLean <andyjmaclean@gmail.com>
2018-11-29 16:25:21 +0100

    EC-3162 Remove per_page param

--- source/js/modules/eu/channels/search-results.js
+++ source/js/modules/eu/channels/search-results.js
@@ -162,6 +162,7 @@ define(['jquery', 'util_scrollEvents', 'eu_data_continuity', 'purl'], function($
     if(typeof window.newRecordPageDesign === 'boolean' && window.newRecordPageDesign){
 
       var page    = $.url(location.href).param('page');
+      var perPage = $.url(location.href).param('per_page');
       var channel = $('.breadcrumbs').data('store-channel-name');
 
       var fnGetText = function($el){
@@ -229,7 +230,7 @@ define(['jquery', 'util_scrollEvents', 'eu_data_continuity', 'purl'], function($
           sessionStorage.eu_portal_last_results_current = current;
         });
 
-        sessionStorage.eu_portal_last_results_page   = page ? page : 1;
+        sessionStorage.eu_portal_last_results_page   = perPage ? perPage : 12;
         sessionStorage.eu_portal_last_results_page   = page ? page : 1;
         sessionStorage.eu_portal_last_results_items  = JSON.stringify(lastResults);
         sessionStorage.eu_portal_last_results_total  = (resInfo.match(/[\d,\,]+(?=\D*$)/) + '').replace(/[\,,\.]/g, '');
